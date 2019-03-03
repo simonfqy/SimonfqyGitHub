08c0f12932bab3dfaf13a3feb543ff497122148f
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-03 15:20:00 -0800

    Update 200_longest_palindromic_substring.java

--- lintcode/medium/200_longest_palindromic_substring.java
+++ lintcode/medium/200_longest_palindromic_substring.java
@@ -34,3 +34,43 @@ public class Solution {
         }
     } 
 }
+
+// Using dynamic programming.
+public class Solution {
+    /**
+     * @param s: input string
+     * @return: the longest palindromic substring
+     */
+   
+    public String longestPalindrome(String s) {
+        // write your code here
+        if (s == null || s.length() == 0)
+            return "";
+        int start = 0;
+        int longest = 0;
+        int n = s.length();
+        boolean[][] is_palindrome_tbl = new boolean[n][n];
+        for (int i = 0; i < n; i++){
+            is_palindrome_tbl[i][i] = true;
+            
+            if (i < 1){
+                continue;
+            }
+            is_palindrome_tbl[i][i-1] = true;
+        }
+        longest = 1;
+        for (int i = n - 1; i >= 0; i--){
+            for (int j = i + 1; j < n; j++){
+                if (s.charAt(i) == s.charAt(j) && is_palindrome_tbl[i+1][j-1] == true){
+                    is_palindrome_tbl[i][j] = true;
+                }
+                if (is_palindrome_tbl[i][j] == true && j - i + 1 > longest){
+                    longest = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        
+        return s.substring(start, start + longest);
+    }    
+}
