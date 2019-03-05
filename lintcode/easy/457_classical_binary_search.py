6b7b800f9b790c033daee5235a58e8967074d0ee
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-04 17:27:28 -0800

    Update 457_classical_binary_search.py

--- lintcode/easy/457_classical_binary_search.py
+++ lintcode/easy/457_classical_binary_search.py
@@ -57,3 +57,29 @@ class Solution:
         if nums[start] == target:
             return start
         return -1
+
+    
+# This solution uses recursion.
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def findPosition(self, nums, target):
+        # write your code here
+        if not nums or not len(nums):
+            return -1
+        return self.binary_search(nums, 0, len(nums) - 1, target)
+    
+    
+    def binary_search(self, nums, start, end, target):
+        if start > end:
+            return -1
+        mid = (start + end) // 2
+        if nums[mid] > target:
+            return self.binary_search(nums, start, mid - 1, target)
+        if nums[mid] == target:
+            return mid
+        if nums[mid] < target:
+            return self.binary_search(nums, mid + 1, end, target)
