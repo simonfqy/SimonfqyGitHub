4ff160f2696e98e149a0555c625a26933061c77b
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-05 16:20:29 -0800

    Update 460_find_k_closest_elements.java

--- lintcode/medium/460_find_k_closest_elements.java
+++ lintcode/medium/460_find_k_closest_elements.java
@@ -65,3 +65,65 @@ public class Solution {
         return -1;
     }
 }
+
+
+// This is my own implementation after the solution from Jiuzhang.com.
+public class Solution {
+    /**
+     * @param A: an integer array
+     * @param target: An integer
+     * @param k: An integer
+     * @return: an integer array
+     */
+    public int[] kClosestNumbers(int[] A, int target, int k) {
+        // write your code here
+        
+        int[] outputList = new int[k];
+        if (k <= 0 || A.length <= 0){
+            return outputList;
+        }
+        int left = getLowerClosest(A, target);
+        int right = left + 1;
+        for (int i = 0; i < k; i++){
+            if (isLeftCloser(A, left, right, target)){
+                outputList[i] = A[left];
+                left--;
+            }
+            else{
+                outputList[i] = A[right];
+                right++;
+            }
+        }
+        return outputList;
+    }
+    
+    public int getLowerClosest(int[] A, int target){
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (A[mid] <= target){
+                start = mid;
+            }
+            else{
+                end = mid;
+            }
+        }
+        if (A[end] <= target){
+            return end;
+        }
+        if (A[start] <= target){
+            return start;
+        }
+        return -1;
+    }
+    
+    public boolean isLeftCloser(int[] A, int left, int right, int target){
+        if (left < 0){
+            return false;
+        }
+        if (right >= A.length){
+            return true;
+        }
+        return target - A[left] <= A[right] - target;
+    }
+}
