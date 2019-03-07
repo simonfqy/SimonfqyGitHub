c23b08d23adcd0b03643717e48bb320a26b0075a
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-06 16:50:50 -0800

    Update 28_search_a_2D_matrix.py

--- lintcode/easy/28_search_a_2D_matrix.py
+++ lintcode/easy/28_search_a_2D_matrix.py
@@ -54,3 +54,40 @@ class Solution:
             else:
                 start = mid
         return start, end, False    
+    
+
+# 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+
+class Solution:
+    """
+    @param matrix, a list of lists of integers
+    @param target, an integer
+    @return a boolean, indicate whether matrix contains target
+    """
+    def searchMatrix(self, matrix, target):
+        if len(matrix) == 0:
+            return False
+            
+        n, m = len(matrix), len(matrix[0])
+        start, end = 0, n * m - 1
+        while start + 1 < end:
+            mid = (start + end) / 2
+            x, y = mid / m, mid % m
+            if matrix[x][y] < target:
+                start = mid
+            else:
+                end = mid
+        x, y = start / m, start % m
+        if matrix[x][y] == target:
+            return True
+        
+        x, y = end / m, end % m
+        if matrix[x][y] == target:
+            return True
+        
+        return False
