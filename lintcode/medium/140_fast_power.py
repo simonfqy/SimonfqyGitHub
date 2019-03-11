e020a6f8c103a8c1f30ac6e0c48fe6aeb1036569
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-11 16:41:03 -0700

    Update 140_fast_power.py

--- lintcode/medium/140_fast_power.py
+++ lintcode/medium/140_fast_power.py
@@ -45,3 +45,31 @@ class Solution:
             ret_value = int((inner_mod * (ret_value % b)) % b)
             n -= exponent
         return ret_value
+    
+    
+# 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+class Solution:
+    """
+    @param a, b, n: 32bit integers
+    @return: An integer
+    """
+    def fastPower(self, a, b, n):
+        # a ^ n % b
+        # 比如 n=5,可以看做 a^(101)2 % b （5的二进制是101）
+        # 拆开也就是 [a^(100)2 * a&(1)2] % b
+        # 因此相当于我们把 n 做二进制转换，碰到 1 的时候，乘一下对应的 a 的幂次
+        # 而 a 的幂次我们只需要知道 a^1, a^(10)2, a^(100)2 ... 也就是 a^1, a^2, a^4 ...
+        # 因此不断的把 a = a * a 就可以了
+        # 中间计算的时候，随时可以 % b 避免 overflow 其不影响结果，这是 % 运算的特性。
+        ans = 1
+        while n > 0:
+            if n % 2 == 1:
+                ans = (ans * a) % b
+            a = a * a % b
+            n = n // 2
+        return ans % b
