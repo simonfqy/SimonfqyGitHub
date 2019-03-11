dc7a51585ca35e7df515c23c27bbe1c37b783705
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-11 15:36:08 -0700

    Update 140_fast_power.py

--- lintcode/medium/140_fast_power.py
+++ lintcode/medium/140_fast_power.py
@@ -19,3 +19,29 @@ class Solution:
             return int(((a % b) * pow(self.fastPower(a, b, (n - 1) / 2), 2)) % b)
         else:
             return int((pow(self.fastPower(a, b, n/2), 2)) % b)
+      
+    
+# My own non-recursive solution.
+class Solution:
+    """
+    @param a: A 32bit integer
+    @param b: A 32bit integer
+    @param n: A 32bit integer
+    @return: An integer
+    """
+    def fastPower(self, a, b, n):
+        # write your code here
+        # Try non-recursive version
+        if n == 0:
+            return int(1 % b)
+        ret_value = int(a % b)
+        n -= 1
+        while n > 0:
+            exponent = 1
+            inner_mod = int(a % b)
+            while exponent <= n / 2:
+                inner_mod = int(pow(inner_mod, 2) % b)
+                exponent *= 2
+            ret_value = int((inner_mod * (ret_value % b)) % b)
+            n -= exponent
+        return ret_value
