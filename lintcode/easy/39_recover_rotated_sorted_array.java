9ee1826590b5be995d9fc0e53bca09a9682724ea
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-11 23:11:00 -0700

    Create 39_recover_rotated_sorted_array.java

---
+++ lintcode/easy/39_recover_rotated_sorted_array.java
@@ -0,0 +1,37 @@
+/**
+* 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
+* - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+* - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+* - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+* - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+*/ 
+
+import java.util.ArrayList;
+
+
+public class Solution {
+    /**
+     * @param nums: The rotated sorted array
+     * @return: The recovered sorted array
+     */
+    private void reverse(ArrayList<Integer> nums, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            int temp = nums.get(i);
+            nums.set(i, nums.get(j));
+            nums.set(j, temp);
+        }
+    }
+
+    public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
+        for (int index = 0; index < nums.size() - 1; index++) {
+            //找到第一个比后面的数大的数，以[4,5,1,2,3]为例，找到5，翻转[4,5]得到[5,4]，翻转[1,2,3]得到[3,2,1]
+            //最后翻转[5,4,3,2,1]得到[1,2,3,4,5]
+            if (nums.get(index) > nums.get(index + 1)) {
+                reverse(nums, 0, index);
+                reverse(nums, index + 1, nums.size() - 1);
+                reverse(nums, 0, nums.size() - 1);
+                return;
+            }
+        }
+    }
+}
