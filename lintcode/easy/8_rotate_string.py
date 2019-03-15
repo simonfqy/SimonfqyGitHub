a1cf8baa4951de18d16f0b5b50f4379cf99cc6d1
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-14 19:02:08 -0700

    Update 8_rotate_string.py

--- lintcode/easy/8_rotate_string.py
+++ lintcode/easy/8_rotate_string.py
@@ -27,3 +27,25 @@ class Solution:
             str[end] = temp
             start += 1
             end -= 1
+            
+            
+# 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+# This solution is not so good. It does not have constant space complexity.
+class Solution:
+    # @param s: a list of char
+    # @param offset: an integer 
+    # @return: nothing
+    def rotateString(self, s, offset):
+        # write you code here
+        if len(s) > 0:
+            offset = offset % len(s)
+            
+        temp = (s + s)[len(s) - offset : 2 * len(s) - offset]
+
+        for i in xrange(len(temp)):
+            s[i] = temp[i]
