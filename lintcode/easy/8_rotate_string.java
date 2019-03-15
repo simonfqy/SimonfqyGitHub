dc2897a521bdf5a8143e922e781bcc4e43923f08
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-14 19:18:39 -0700

    Create 8_rotate_string.java

---
+++ lintcode/easy/8_rotate_string.java
@@ -0,0 +1,60 @@
+public class Solution {
+    /**
+     * @param str: An array of char
+     * @param offset: An integer
+     * @return: nothing
+     */
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+        int lenStr = str.length;
+        if (str == null || lenStr <= 1){
+            return;
+        }
+        offset = offset % lenStr;
+        reverse(str, 0, lenStr - 1);
+        reverse(str, 0, offset - 1);
+        reverse(str, offset, lenStr - 1);
+    }
+    
+    public void reverse(char[] str, int start, int end){
+        for (int i = start, j = end; i < j; i++, j--){
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+        }
+    }
+}
+
+/**
+* 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
+* - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+* - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+* - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+* - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+*/ 
+
+public class Solution {
+    /**
+     * @param str: an array of char
+     * @param offset: an integer
+     * @return: nothing
+     */
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+        if (str == null || str.length == 0)
+            return;
+            
+        offset = offset % str.length;
+        reverse(str, 0, str.length - offset - 1);
+        reverse(str, str.length - offset, str.length - 1);
+        reverse(str, 0, str.length - 1);
+    }
+    
+    private void reverse(char[] str, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+        }
+    }
+}
