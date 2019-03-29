e6c7c08d3985b60c04fc2e0e0245393256afcbc6
Qingyuan Feng <simonfengqy@gmail.com>
2019-03-28 19:13:48 -0700

    Update 140_fast_power.java

--- lintcode/medium/140_fast_power.java
+++ lintcode/medium/140_fast_power.java
@@ -28,3 +28,34 @@ public class Solution {
         return (int) ans % b;
     }
 }
+
+
+// My own solution.
+public class Solution {
+    /**
+     * @param a: A 32bit integer
+     * @param b: A 32bit integer
+     * @param n: A 32bit integer
+     * @return: An integer
+     */
+    public int fastPower(int a, int b, int n) {
+        // write your code here
+        if (n == 0){
+            return (1 % b);
+        }
+        if (n == 1){
+            return (a % b);
+        }
+        long temp = 0;
+        if (n % 2 == 1){
+            temp = fastPower(a, b, (n-1)/2);
+            return (int) ((((temp * temp) % b) * a) % b);
+        }
+        else{
+            temp = fastPower(a, b, n/2);
+            // It is extremely important to wrap it in suitable brackets. The modulo operator
+            // has a pretty low precedence.
+            return (int) ((temp * temp) % b);
+        }
+    }
+}
