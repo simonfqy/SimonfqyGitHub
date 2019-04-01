74ee6789b0dbf07d954379905ff89b0be06f2485
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-01 16:09:06 -0700

    Update 56_two_sum.java

--- lintcode/easy/56_two_sum.java
+++ lintcode/easy/56_two_sum.java
@@ -52,3 +52,27 @@ public class Solution {
         return res;
     }
 }
+
+
+// Kind of copied from Jiuzhang.com.
+public class Solution {
+    /**
+     * @param numbers: An array of Integer
+     * @param target: target = numbers[index1] + numbers[index2]
+     * @return: [index1, index2] (index1 < index2)
+     */
+    
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++){
+            if (map.get(target - numbers[i]) != null){
+                int[] pair = {map.get(target - numbers[i]), i};
+                return pair;
+            }
+            map.put(numbers[i], i);
+        }
+        int[] result = {};
+        return result;
+    }
+}
