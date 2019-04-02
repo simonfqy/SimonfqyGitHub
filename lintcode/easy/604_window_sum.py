e6d18d70d72dce2c37a89802f78abb95eed59348
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-01 19:08:27 -0700

    Update 604_window_sum.py

--- lintcode/easy/604_window_sum.py
+++ lintcode/easy/604_window_sum.py
@@ -23,3 +23,22 @@ class Solution:
             sum_val = sum_val - nums[i] + nums[i + k]
             res.append(sum_val)
         return res
+
+# A faster solution. The official one from Jiuzhang.com.    
+class Solution:
+    """
+    @param nums: a list of integers.
+    @param k: length of window.
+    @return: the sum of the element inside the window at each moving.
+    """
+    def winSum(self, nums, k):
+        # write your code here
+        n = len(nums)
+        if n < k or k <= 0:
+            return []
+        res = [0] * (n - k + 1)
+        res[0] = sum(nums[:k])
+        for i in range(0, n - k):
+            res[i + 1] = res[i] - nums[i] + nums[i + k]
+            
+        return res
