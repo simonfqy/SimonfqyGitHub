39d6ca49888a77811eb5490f0c6cb05205396d82
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-01 18:06:27 -0700

    Update 521_remove_duplicate_numbers_in_array.java

--- lintcode/easy/521_remove_duplicate_numbers_in_array.java
+++ lintcode/easy/521_remove_duplicate_numbers_in_array.java
@@ -20,3 +20,26 @@ public class Solution {
         return left;
     }
 }
+
+
+// Solution from Jiuzhang.com. It is unnecessarily complicated since we actually only need set.
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: the number of unique integers
+     */
+    public int deduplication(int[] nums) {
+        // write your code here
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i], true);
+        }
+        int result = 0;
+        // Either HashMap.Entry<Integer, Boolean> or Map.Entry<Integer, Boolean> is correct.
+        for (HashMap.Entry<Integer, Boolean> entry : map.entrySet()){
+            nums[result] = entry.getKey();
+            result++;
+        }
+        return result;
+    }
+}
