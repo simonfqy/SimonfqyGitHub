f52c880caaf37488b233a76901b24abc556b38f7
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-02 18:28:44 -0700

    Update sorting.py

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -7,5 +7,16 @@ def quick_sort(A, start, end):
     if start >= end:
         return
     left, right = start, end
-    if left <= right:
-        
+    pivot = A[(start + end) // 2]
+    while left <= right:
+        while left <= right and A[left] < pivot:
+            left += 1
+        while left <= right and A[right] < pivot:
+            right -= 1
+        if left <= right:
+            A[left], A[right] = A[right], A[left]
+            left += 1
+            right -= 1
+            
+    quick_sort(A, start, right)
+    quick_sort(A, left, end)
