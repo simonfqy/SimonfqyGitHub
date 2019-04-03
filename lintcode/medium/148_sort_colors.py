424f0aa686f99ec82f63d62f529dd0301cee3b36
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-03 15:32:26 -0700

    Update 148_sort_colors.py

--- lintcode/medium/148_sort_colors.py
+++ lintcode/medium/148_sort_colors.py
@@ -25,3 +25,34 @@ class Solution:
             else:
                 middle += 1
         return
+    
+    
+# This solution is not as good since it involves traversing the array twice.   
+# 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+
+class Solution:
+    """
+    @param nums: A list of integer which is 0, 1 or 2 
+    @return: nothing
+    """
+    def sortColors(self, A):
+        index = self.sort(A, 0, 0)
+        self.sort(A, 1, index)
+        
+    def sort(self, A, flag, index):
+        start, end = index, len(A) - 1
+        while start <= end:
+            while start <= end and A[start] == flag:
+                start += 1
+            while start <= end and A[end] != flag:
+                end -= 1
+            if start <= end:
+                A[start], A[end] = A[end], A[start]
+                start += 1
+                end -= 1
+        return start
