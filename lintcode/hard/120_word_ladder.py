bc95b70ef9f075e37e3bc8aaaa26eb755a64d5be
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-06 14:23:28 -0700

    Create 120_word_ladder.py

---
+++ lintcode/hard/120_word_ladder.py
@@ -0,0 +1,54 @@
+'''
+Link: https://www.lintcode.com/problem/word-ladder/description
+'''
+
+# This solution is almost copied from Jiuzhang.com
+from collections import deque
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: An integer
+    """
+    
+    def ladderLength(self, start, end, dict):
+        # write your code here
+        
+        # This dict.add(end) is crucial. Otherwise all inputs will get 0 returned.
+        dict.add(end)
+        length = 0
+        visited = set([start])
+        queue = deque([start])
+        while queue:
+            length += 1
+            for _ in range(len(queue)):
+                this_word = queue.popleft()
+                
+                if this_word == end:
+                    return length
+                
+                for word in self.get_next_words(this_word):
+                    if word not in dict:
+                        continue
+                    if word in visited:
+                        continue
+                    queue.append(word)
+                    # Be aware of the position of this visited.add(). If it is put after this_word=queue.popleft(),
+                    # it will cause runtime exceeded anomaly. It is because in that case, the visited set is not
+                    # populated fast enough, many words are unnecessarily visited more than once. So populate the 
+                    # visited set early can avoid this problem. Always remember.
+                    visited.add(word)
+                    
+        return 0
+        
+        
+    def get_next_words(self, word):
+        words = []
+        for i in range(len(word)):
+            left, right = word[:i], word[i+1:]
+            for char in "abcdefghijklmnopqrstuvwxyz":
+                if char == word[i]:
+                    continue
+                words.append(left + char + right)
+        return words
