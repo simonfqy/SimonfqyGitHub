a89ef5ba45026aa38ea46ab19cb992baeeab4943
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-11 20:34:46 -0700

    Update 461_kth_smallest_numbers_in_unsorted_array.py

--- lintcode/medium/461_kth_smallest_numbers_in_unsorted_array.py
+++ lintcode/medium/461_kth_smallest_numbers_in_unsorted_array.py
@@ -38,3 +38,44 @@ class Solution:
         if k >= left:
             return self.quick_select(k, nums, left, end)
         return nums[k]
+    
+    
+# 本参考程序来自九章算法，由 @九章算法助教团队 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+
+class Solution:
+    """
+    @param k: An integer
+    @param nums: An integer array
+    @return: kth smallest element
+    """
+    def kthSmallest(self, k, nums):
+        return self.quickSelect(nums, 0, len(nums) - 1, k - 1)
+    
+    def quickSelect(self, nums, start, end, k):
+        if start == end:
+            return nums[start]
+        
+        left, right = start, end
+        pivot = nums[(left + right) // 2]
+        
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] > pivot:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+                
+        if right >= k and start <= right:
+            return self.quickSelect(nums, start, right, k)
+        elif left <= k and left <= end:
+            return self.quickSelect(nums, left, end, k)
+        else:
+            return nums[k]
