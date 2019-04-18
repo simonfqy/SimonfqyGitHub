54fb495262700bef084690341a648d8f9af349dc
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-17 23:49:12 -0700

    Update 143_sort_colors_ii.py

--- lintcode/medium/143_sort_colors_ii.py
+++ lintcode/medium/143_sort_colors_ii.py
@@ -44,3 +44,67 @@ class Solution:
         
         self.quicker_sort(colors, start_color, pivot, left_ind, right)
         self.quicker_sort(colors, pivot, end_color, left, right_ind)
+        
+        
+# 本参考程序来自九章算法，由 @令狐冲 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+
+class Solution:
+    """
+    @param colors: A list of integer
+    @param k: An integer
+    @return: nothing
+    """
+    def sortColors2(self, colors, k):
+        self.sort(colors, 1, k, 0, len(colors) - 1)
+        
+    def sort(self, colors, color_from, color_to, index_from, index_to):
+        if color_from == color_to or index_from == index_to:
+            return
+            
+        color = (color_from + color_to) // 2
+        
+        left, right = index_from, index_to
+        while left <= right:
+            while left <= right and colors[left] <= color:
+                left += 1
+            while left <= right and colors[right] > color:
+                right -= 1
+            if left <= right:
+                colors[left], colors[right] = colors[right], colors[left]
+                left += 1
+                right -= 1
+        
+        self.sort(colors, color_from, color, index_from, right)
+        self.sort(colors, color + 1, color_to, left, index_to)
+        
+
+# According to the students, this version is also correct:
+def sortColors2(self, colors, k):
+    # write your code here
+    self.rainbowSort(1, k, 0, len(colors) - 1, colors)
+
+def rainbowSort(self, color_start, color_end, start, end, colors):
+    if color_start >= color_end or start >= end:
+        return
+
+    pivot = (color_start + color_end) // 2 + 1
+    left = start
+    right = end
+
+    while left <= right:
+        while left <= right and colors[left] < pivot:
+            left += 1
+        while left <= right and colors[right] >= pivot:
+            right -= 1
+        if left <= right:
+            colors[left], colors[right] = colors[right], colors[left]
+            left += 1
+            right -= 1
+
+    self.rainbowSort(color_start, pivot - 1, start, right, colors)
+    self.rainbowSort(pivot, color_end, left, end, colors)
