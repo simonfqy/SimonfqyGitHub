f27666349b0994dad9f3b9b73b2debff0fc6e604
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-18 17:38:31 -0700

    Update 380_intersection_of_two_linked_lists.py

--- lintcode/medium/380_intersection_of_two_linked_lists.py
+++ lintcode/medium/380_intersection_of_two_linked_lists.py
@@ -35,3 +35,43 @@ class Solution:
                 visited.add(b_ptr)
                 b_ptr = b_ptr.next
         return None
+
+ 
+# This solution is based on a hint in the discussion section. Although it works correctly,
+# its problem is that the time limit is easily exceeded.
+class Solution:
+    """
+    @param headA: the first list
+    @param headB: the second list
+    @return: a ListNode
+    """
+    def getIntersectionNode(self, headA, headB):
+        # write your code here
+        if headA is None or headB is None:
+            return None
+        a_ptr, b_ptr = headA, headB
+        len_a, len_b = 0, 0
+        a_cycle, b_cycle = 0, 0
+        while True:
+            if a_ptr.val == b_ptr.val:
+                return a_ptr
+            if a_cycle == 0:
+                len_a += 1
+            if b_cycle == 0:
+                len_b += 1
+            if a_ptr.next is not None:
+                a_ptr = a_ptr.next
+            else:
+                a_ptr = headA
+                a_cycle += 1
+                if b_cycle >= 1 and (a_cycle * len_a) % len_b == 0:
+                    return None
+            
+            if b_ptr.next is not None:
+                b_ptr = b_ptr.next
+            else:
+                b_ptr = headB
+                b_cycle += 1
+                if a_cycle >= 1 and (b_cycle * len_b) % len_a == 0:
+                    return None
+        return None
