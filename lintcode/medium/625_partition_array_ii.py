bc225ebe007b2f4dda9d95115e0a0d4e4c716de5
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-21 22:40:43 -0700

    Update 625_partition_array_ii.py

--- lintcode/medium/625_partition_array_ii.py
+++ lintcode/medium/625_partition_array_ii.py
@@ -3,6 +3,8 @@ Link: https://www.lintcode.com/problem/partition-array-ii/description
 '''
 
 # I did not completely come up with this solution correctly. I had to refer to the sort colors problem solution.
+# TAKEAWAY lesson: if not sure, do things one step at a time. Use logic to eliminate impossible but confusing and
+# annoying cases.
 class Solution:
     """
     @param nums: an integer array
@@ -18,6 +20,11 @@ class Solution:
         while mid <= right:
             # The whole if-else block only traverses 1 step per iteration of the while loop.
             if nums[mid] < low:
+                # Since the mid pointer traverses the array from the start, to this point, all entries
+                # prior to mid are smaller than or equal to "high". What if after swapping nums[left] and 
+                # nums[mid], the new nums[mid] is greater than high and since mid is now mid+1, the array
+                # will be in the wrong order? This case will not happen, rest assured. 
+                # WE SHOULD ALWAYS USE LOGIC TO ELIMINATE THE IMPOSSIBLE CASES.
                 nums[left], nums[mid] = nums[mid], nums[left]
                 left += 1
                 # Naturally, move the middle pointer forward. 
