af8c6a2cac7c4f2082e6eedbb8f4bd792c3c2870
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-26 16:44:39 -0700

    Update 67_binary_tree_inorder_traversal.py

--- lintcode/easy/67_binary_tree_inorder_traversal.py
+++ lintcode/easy/67_binary_tree_inorder_traversal.py
@@ -35,3 +35,44 @@ class Solution:
                 node = node.right
             
         return output_list
+    
+    
+# 本参考程序来自九章算法，由 @社会我喵哥 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+
+"""
+九章标准答案iterate java版本的python 翻译
+
+挪到下一个点的算法如下:
+• 如果当前点存在右子树，那么就是右子树中“一路向西”最左边的那个点
+• 如果当前点不存在右子树，则是走到当前点的路径中，第一个左拐的点
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Inorder in ArrayList which contains node values.
+    """
+    def inorderTraversal(self, root):
+        stack = []
+        result = []
+        while root:
+            stack.append(root)
+            root = root.left
+        while stack:
+            node = stack[-1]
+            result.append(node.val)
+            if node.right: 
+                node = node.right
+                while node:
+                    stack.append(node)
+                    node = node.left
+            else:
+                node = stack.pop()
+                while stack and stack[-1].right == node:
+                    node = stack.pop()
+        return result
