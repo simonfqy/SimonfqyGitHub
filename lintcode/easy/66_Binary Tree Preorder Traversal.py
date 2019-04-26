f6f5fa3f587454c41c8ed85197dd03e39d776623
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-25 21:08:31 -0700

    Update 66_Binary Tree Preorder Traversal.py

--- lintcode/easy/66_Binary Tree Preorder Traversal.py
+++ lintcode/easy/66_Binary Tree Preorder Traversal.py
@@ -26,3 +26,28 @@ class Solution:
             if node.left:
                 stack.append(node.left)
         return output_list
+    
+    
+# My own solution, 3 months after the previous one.
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        # write your code here
+        value_list = []
+        if root is None:
+            return value_list
+        stack = [root]
+        node = root
+        while stack:
+            if node is not None:
+                value_list.append(node.val)
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                node = node.right
+        
+        return value_list
