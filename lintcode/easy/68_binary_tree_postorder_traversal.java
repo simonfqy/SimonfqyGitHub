d28abbba96538f8010f72325c1a55c9eba4efe83
Qingyuan Feng <simonfengqy@gmail.com>
2019-04-29 19:05:19 -0700

    Update 68_binary_tree_postorder_traversal.java

--- lintcode/easy/68_binary_tree_postorder_traversal.java
+++ lintcode/easy/68_binary_tree_postorder_traversal.java
@@ -61,3 +61,64 @@ public List<Integer> postorderTraversal(TreeNode root) {
         
         return list;
     }
+
+
+// The following solution is conceptually similar to the last one.
+/**
+* 本参考程序来自九章算法，由 @Z同学 提供。版权所有，转发请注明出处。
+* - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+* - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+* - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+* - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+*/ 
+
+public class Solution {
+    
+    public enum Operation {
+        RPOCESS, 
+        ADDTORESULT
+    }
+    
+    /**
+     * @param root: A Tree
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Operation> operationStack = new Stack<>();
+        
+        stack.push(root);
+        operationStack.push(Operation.RPOCESS);
+        
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            Operation operation = operationStack.pop();
+            
+            if (node == null) {
+                continue;
+            }
+            
+            if (operation == Operation.ADDTORESULT) {
+                result.add(node.val);
+            }
+            else {
+                stack.push(node);
+                operationStack.push(Operation.ADDTORESULT);
+                
+                stack.push(node.right);
+                operationStack.push(Operation.RPOCESS);
+                
+                stack.push(node.left);
+                operationStack.push(Operation.RPOCESS);
+            }
+        }
+        
+        return result;
+    }
+}
