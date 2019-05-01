027f72695c288833f9363a789ef3caae715c75ec
Qingyuan Feng <simonfengqy@gmail.com>
2019-05-01 16:42:58 -0700

    Update 95_validate_binary_search_tree.py

--- lintcode/medium/95_validate_binary_search_tree.py
+++ lintcode/medium/95_validate_binary_search_tree.py
@@ -31,3 +31,30 @@ class Solution:
                 prev = node
                 node = node.right
         return True
+
+    
+# Another of my solution. Uses recursive method.
+import math
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+    def isValidBST(self, root):
+        # write your code here
+        if root is None:
+            return True
+        if not self.is_valid(root.left, -math.inf, root.val):
+            return False
+        return self.is_valid(root.right, root.val, math.inf)
+        
+    def is_valid(self, root, lower_bound, upper_bound):
+        if root is None:
+            return True
+        root_val = root.val
+        if root_val <= lower_bound or root_val >= upper_bound:
+            return False
+        return (self.is_valid(root.left, lower_bound, root_val) and self.is_valid(
+            root.right, root_val, upper_bound))
+    
+    
