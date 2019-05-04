3a28107ebd78dd83181b52296be055af83016d36
Qingyuan Feng <simonfengqy@gmail.com>
2019-05-03 18:34:05 -0700

    Update 900_closest_binary_search_tree_value.py

--- lintcode/easy/900_closest_binary_search_tree_value.py
+++ lintcode/easy/900_closest_binary_search_tree_value.py
@@ -31,3 +31,33 @@ class Solution:
             if abs(left_closest - target) < abs(curr_closest - target):
                 return left_closest
         return curr_closest
+
+    
+# 本参考程序来自九章算法，由 @吴助教 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+# Uses iteration instead of recursion. Pretty succinct.
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        upper = root
+        lower = root
+        while root:
+            if target > root.val:
+                lower = root
+                root = root.right
+            elif target < root.val:
+                upper = root
+                root = root.left
+            else:
+                return root.val
+        if abs(upper.val - target) <= abs(lower.val - target):
+            return upper.val
+        return lower.val
