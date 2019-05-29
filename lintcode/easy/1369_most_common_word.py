f89120de16c66601b000b38b7cba7677bd187e10
Qingyuan Feng <simonfengqy@gmail.com>
2019-05-28 21:35:08 -0700

    Update 1369_most_common_word.py

--- lintcode/easy/1369_most_common_word.py
+++ lintcode/easy/1369_most_common_word.py
@@ -35,3 +35,23 @@ class Solution:
         for word, freq in word_to_freq.items():
             if freq == max_freq:
                 return word
+            
+            
+# This solution uses regular expression. Much more powerful than the previous solution.
+# 本参考程序来自九章算法，由 @九章算法助教团队 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+import re
+class Solution:
+    """
+    @param paragraph: 
+    @param banned: 
+    @return: nothing
+    """
+    def mostCommonWord(self, paragraph, banned):
+        ban = set(banned)
+        words = re.findall(r'\w+', paragraph.lower())
+        return collections.Counter(w for w in words if w not in ban).most_common(1)[0][0]
