42cd08a04a7e35a5162deb5289a6fd6d15fec39c
Qingyuan Feng <simonfengqy@gmail.com>
2019-05-29 16:52:57 -0700

    Update 15_permutations.py

--- lintcode/medium/15_permutations.py
+++ lintcode/medium/15_permutations.py
@@ -41,3 +41,27 @@ class Solution:
             self.dfs(nums, ind_availability, set(), perm, output_list)
             ind_availability[i] = True
             this_lvl_unavailable_inds.add(i)
+            
+            
+# My own solution after 7 weeks of writing the previous solution.
+class Solution:
+    """
+    @param: nums: A list of integers.
+    @return: A list of permutations.
+    """
+    def permute(self, nums):
+        # write your code here
+        if nums is None or len(nums) <= 0:
+            return [[]]
+        output_list = []
+        self.get_permutations(nums, [], output_list)
+        return output_list
+        
+    def get_permutations(self, nums, subset, output_list):
+        if len(subset) == len(nums):
+            output_list.append(subset)
+            return
+        for number in nums:
+            if number in set(subset):
+                continue
+            self.get_permutations(nums, subset + [number], output_list)
