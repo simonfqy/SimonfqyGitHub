2d5de7bd3e13d5163003aa14dea35a83eafebd07
Qingyuan Feng <simonfengqy@gmail.com>
2019-05-29 20:02:24 -0700

    Update 16_permutations_ii.py

--- lintcode/medium/16_permutations_ii.py
+++ lintcode/medium/16_permutations_ii.py
@@ -38,7 +38,6 @@ class Solution:
             ind_available[i] = False
             self.dfs(nums, ind_available, perm, output_list)
             ind_available[i] = True
-
             
             
 # This solution is provided by Jiuzhang.com. It also works.
@@ -78,3 +77,39 @@ class Solution:
             ind_available[i] = False
             self.dfs(nums, ind_available, perm, output_list)
             ind_available[i] = True
+            
+            
+# Inspired by Jiuzhang.com.
+class Solution:
+    """
+    @param: :  A list of integers
+    @return: A list of unique permutations
+    """
+
+    def permuteUnique(self, nums):
+        # write your code here
+        output_list = []
+        if nums is None:
+            return output_list
+        nums.sort()
+        used = [False for _ in nums]
+        self.get_permutations(nums, [], used, output_list)
+        return output_list
+        
+    def get_permutations(self, nums, subset, used, output_list):
+        if len(subset) == len(nums):
+            output_list.append(subset)
+            return
+        for i, number in enumerate(nums):
+            # Make sure that the used elements are not duplicated.
+            if used[i]:
+                continue
+            # Make sure that we will not choose the duplicated elements: the previous ones of such duplicated
+            # elements must all appear, if they don't appear, it will result in duplications and such cases 
+            # should be filtered out.
+            if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                continue
+            used[i] = True
+            self.get_permutations(nums, subset + [number], used, output_list)
+            # Restore the original array. Essentially backtracking.
+            used[i] = False
