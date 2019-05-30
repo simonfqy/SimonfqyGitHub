1491f8d29b1228dbe58db16618ad42b4b7afa50a
Qingyuan Feng <simonfengqy@gmail.com>
2019-05-29 18:02:53 -0700

    Update 15_permutations.py

--- lintcode/medium/15_permutations.py
+++ lintcode/medium/15_permutations.py
@@ -65,3 +65,31 @@ class Solution:
             if number in set(subset):
                 continue
             self.get_permutations(nums, subset + [number], output_list)
+            
+
+# My own iterative solution using BFS.
+from collections import deque
+class Solution:
+    """
+    @param: nums: A list of integers.
+    @return: A list of permutations.
+    """
+    def permute(self, nums):
+        # write your code here
+        if nums is None or len(nums) <= 0:
+            return [[]]
+        output_list = []
+        queue = deque([[]])
+        self.get_permutations(nums, output_list, queue)
+        return output_list
+        
+    def get_permutations(self, nums, output_list, queue):
+        while queue:
+            subset = queue.popleft()
+            if len(subset) == len(nums):
+                output_list.append(subset)
+                continue
+            for number in nums:
+                if number in set(subset):
+                    continue
+                queue.append(subset + [number])
