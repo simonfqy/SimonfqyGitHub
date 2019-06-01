1579fe74a28c16d1803153dbf90e0cf71b362384
Qingyuan Feng <simonfengqy@gmail.com>
2019-05-31 17:53:55 -0700

    Update 17_subsets.py

--- lintcode/medium/17_subsets.py
+++ lintcode/medium/17_subsets.py
@@ -117,3 +117,27 @@ class Solution:
                     continue
                 queue.append(subset + [number])
         return subset_list
+    
+
+# Solution from Jiuzhang.com. Makes use of bitwise operation. Does not apply to subsets II, in which duplicate
+# elements exist.
+class Solution:
+    def subsets(self, nums):
+        result = []
+        n = len(nums)
+        nums.sort()
+
+        # 1 << n is 2^n
+        # each subset equals to an binary integer between 0 .. 2^n - 1
+        # 0 -> 000 -> []
+        # 1 -> 001 -> [1]
+        # 2 -> 010 -> [2]
+        # ..
+        # 7 -> 111 -> [1,2,3]
+        for i in range(1 << n):
+            subset = []
+            for j in range(n):
+                if (i & (1 << j)) != 0:
+                    subset.append(nums[j])
+            result.append(subset)
+        return result
