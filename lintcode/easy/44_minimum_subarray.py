e071e784c41ae35a31f7e71955e9aa623d37249b
Qingyuan Feng <simonfengqy@gmail.com>
2019-06-02 20:34:36 -0700

    Update 44_minimum_subarray.py

--- lintcode/easy/44_minimum_subarray.py
+++ lintcode/easy/44_minimum_subarray.py
@@ -22,3 +22,24 @@ class Solution:
             max_sum = max(max_sum, prefix_sum)
             
         return min_sum
+
+    
+# Greedy algorithm.    
+class Solution:
+    """
+    @param: nums: a list of integers
+    @return: A integer indicate the sum of minimum subarray
+    """
+    def minSubArray(self, nums):
+        # write your code here
+        curr_sum = 0
+        min_sum = None
+        # A minimum subarray will not begin or end with a positive-sum subarray.
+        for number in nums:
+            curr_sum += number
+            if min_sum is None:
+                min_sum = curr_sum
+            min_sum = min(min_sum, curr_sum)            
+            curr_sum = min(0, curr_sum)
+            
+        return min_sum
