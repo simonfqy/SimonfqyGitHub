fa590dbadb4e9c4c14fa9045772ddb2f9ef07cc5
Qingyuan Feng <simonfengqy@gmail.com>
2019-06-02 20:14:57 -0700

    Update 41_maximum_subarray.py

--- lintcode/easy/41_maximum_subarray.py
+++ lintcode/easy/41_maximum_subarray.py
@@ -48,3 +48,29 @@ class Solution:
                 maximum = max(maximum, prefix_sum - smallest)
             smallest = min(smallest, prefix_sum)
         return maximum
+    
+    
+# Greedy Algorithm, derived from a Java solution on jiuzhang.com.   
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A integer indicate the sum of max subarray
+    """
+    def maxSubArray(self, nums):
+        # write your code here
+        if nums is None or len(nums) <= 0:
+            return None
+        curr_sum = 0
+        max_sum = None
+        # If curr_sum >= 0, increment it as normal (in the next iteration); otherwise, reset it to 0 and 
+        # increment it (in the next iteration). The rationale is that, any maximum subarray with length > 1
+        # will not begin or end with a negative-sum subarray. 
+        for number in nums:
+            curr_sum += number
+            if max_sum is None:
+                max_sum = curr_sum
+            else:
+                max_sum = max(curr_sum, max_sum)
+            curr_sum = max(0, curr_sum)
+            
+        return max_sum
