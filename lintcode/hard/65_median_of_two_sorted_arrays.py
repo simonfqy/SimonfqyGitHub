2e4638d06d032996f5b8d472ec0b63dc0eeaf113
Qingyuan Feng <simonfengqy@gmail.com>
2019-06-04 15:50:53 -0700

    Update 65_median_of_two_sorted_arrays.py

--- lintcode/hard/65_median_of_two_sorted_arrays.py
+++ lintcode/hard/65_median_of_two_sorted_arrays.py
@@ -170,7 +170,6 @@ class Solution:
 # - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
 # - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
 
-
 class Solution:
     """
     @param A: An integer array.
@@ -200,3 +199,76 @@ class Solution:
         if b is None or (a is not None and a < b):
             return self.findKth(A, index_a + k // 2, B, index_b, k - k // 2)
         return self.findKth(A, index_a, B, index_b + k // 2, k - k // 2)
+    
+    
+# A tricky solution.     
+# 本参考程序来自九章算法，由 @李助教 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+# 二分答案的办法
+'''
+算法描述：
+1. 我们需要先确定二分的上下界限，由于两个数组 A, B 均有序，所以下界为 min(A[0], B[0])，上界为 max(A[A.length - 1], B[B.length - 1]).
+2. 判断当前上下界限下的 mid(mid = (start + end) / 2) 是否为我们需要的答案；这里我们可以分别对两个数组进行二分来找到两个数组中小于等于
+   当前 mid 的数的个数 cnt1 与 cnt2，sum = cnt1 + cnt2 即为 A 跟 B 合并后小于等于当前mid的数的个数.
+3. 如果 sum < k，即中位数肯定不是 mid，应该大于 mid，更新 start 为 mid，否则更新 end 为 mid，之后再重复第二步。
+4. 当不满足 start + 1 < end 这个条件退出二分循环时，再分别判断一下 start 跟 end ，最终返回符合要求的那个数即可。
+'''
+
+class Solution:
+    """
+    @param: A: An integer array
+    @param: B: An integer array
+    @return: a double whose format is *.5 or *.0
+    """
+    def findMedianSortedArrays(self, A, B):
+        # write your code here
+        len_a, len_b = len(A), len(B)
+        if (len_a + len_b) % 2 == 1:
+            return self.finf_kth(A, B, (len_a + len_b) // 2 + 1)
+        else:
+            left = self.finf_kth(A, B, (len_a + len_b) // 2 )
+            right = self.finf_kth(A, B, (len_a + len_b) // 2 + 1)
+            return (left + right) / 2
+            
+    def finf_kth(self, A, B, k):
+        if len(A) == 0:
+            left, right = B[0], B[-1]
+        elif len(B) == 0:
+            left, right = A[0], A[-1]
+        else:
+            left, right = min(A[0], B[0]), max(A[-1], B[-1])
+        # The tricky part of this solution is that, left and right are not indices, but values.
+        while left + 1 < right:
+            mid = (left + right) // 2
+            count1 = self.helper(A, mid)
+            count2 = self.helper(B, mid)
+            if count1 + count2 < k:
+                left = mid
+            else:
+                right = mid
+        count1 = self.helper(A, left)
+        count2 = self.helper(B, left)
+        if count1 + count2 >= k:
+            return left
+        else:
+            return right
+    
+    def helper(self, array, flag):
+        if len(array) == 0:
+            return 0
+        left, right = 0 ,len(array) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if array[mid] <= flag:
+                left = mid
+            else:
+                right = mid
+        if array[right] <= flag:
+            return right + 1
+        if array[left] <= flag:
+            return left + 1
+        return 0
