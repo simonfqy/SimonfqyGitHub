9f209c650cc2bbf78a67da20ce69a651f02f211a
Qingyuan Feng <simonfengqy@gmail.com>
2019-06-03 17:45:47 -0700

    Update 65_median_of_two_sorted_arrays.py

--- lintcode/hard/65_median_of_two_sorted_arrays.py
+++ lintcode/hard/65_median_of_two_sorted_arrays.py
@@ -118,3 +118,42 @@ class Solution:
         # new_target_order = target_num_order - (mid_pos + 1) * 2
         # return self.find_element_of_sorted_arrays(A, B, a_start + mid_pos + 1, b_start + mid_pos + 1, \
         #     new_target_order)
+        
+        
+# The original version of the previous solution on jiuzhang.com.
+# 本参考程序来自九章算法，由 @令狐冲 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+
+class Solution:
+    """
+    @param A: An integer array.
+    @param B: An integer array.
+    @return: a double whose format is *.5 or *.0
+    """
+    def findMedianSortedArrays(self, A, B):
+        n = len(A) + len(B)
+        if n % 2 == 1:
+            return self.findKth(A, 0, B, 0, n // 2 + 1)
+        else:
+            smaller = self.findKth(A, 0, B, 0, n // 2)
+            bigger = self.findKth(A, 0, B, 0, n // 2 + 1)
+            return (smaller + bigger) / 2
+
+    def findKth(self, A, index_a, B, index_b, k):
+        if len(A) == index_a:
+            return B[index_b  + k - 1]
+        if len(B) == index_b:
+            return A[index_a + k - 1]
+        if k == 1:
+            return min(A[index_a], B[index_b])
+        
+        a = A[index_a + k // 2 - 1] if index_a + k // 2 <= len(A) else None
+        b = B[index_b + k // 2 - 1] if index_b + k // 2 <= len(B) else None
+        
+        if b is None or (a is not None and a < b):
+            return self.findKth(A, index_a + k // 2, B, index_b, k - k // 2)
+        return self.findKth(A, index_a, B, index_b + k // 2, k - k // 2)
