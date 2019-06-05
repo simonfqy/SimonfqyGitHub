e875b78b11f4948326c5b2be9daf7050570b1ec5
Qingyuan Feng <simonfengqy@gmail.com>
2019-06-04 19:04:25 -0700

    Create 30_insert_interval.py

---
+++ lintcode/medium/30_insert_interval.py
@@ -0,0 +1,66 @@
+'''
+Link: https://www.lintcode.com/problem/insert-interval/description
+'''
+
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: Sorted interval list.
+    @param newInterval: new interval.
+    @return: A new interval list.
+    """
+    def insert(self, intervals, newInterval):
+        # write your code here
+        new_left = newInterval.start
+        new_right = newInterval.end
+        if len(intervals) == 0:
+            return [newInterval]
+        new_intervals = []
+        exist_modified_intervals = False
+        new_interval_added = False
+        for intvl in intervals:
+            left = intvl.start
+            right = intvl.end
+            if left <= new_left and right >= new_right:
+                return intervals
+            if right < new_left:
+                new_intervals.append(intvl)
+                continue
+            if left > new_right:
+                if not exist_modified_intervals and not new_interval_added:
+                    new_intervals.append(newInterval)
+                new_intervals.append(intvl)
+                new_interval_added = True
+                continue
+            if left <= new_left and right >= new_left and right < new_right:
+                # create a new, larger interval.
+                merged_intvl = Interval(left, new_right)
+                new_intervals.append(merged_intvl)
+                exist_modified_intervals = True
+                continue
+            if left >= new_left and right <= new_right:
+                # Included in the new list.
+                if exist_modified_intervals:
+                    continue
+                # Append new.
+                new_intervals.append(newInterval)
+                exist_modified_intervals = True
+                continue
+            if left <= new_right and right > new_right:
+                if exist_modified_intervals:
+                    new_intervals[-1].end = right
+                    continue
+                merged_intvl = Interval(new_left, right)
+                new_intervals.append(merged_intvl)
+                exist_modified_intervals = True
+                continue
+        if not new_interval_added and not exist_modified_intervals:
+            new_intervals.append(newInterval)
+        return new_intervals
