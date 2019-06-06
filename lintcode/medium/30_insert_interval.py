66e4fe5ebc7867b08ead2f1f03c574556b6f7e11
Qingyuan Feng <simonfengqy@gmail.com>
2019-06-05 19:43:19 -0700

    Update 30_insert_interval.py

--- lintcode/medium/30_insert_interval.py
+++ lintcode/medium/30_insert_interval.py
@@ -130,3 +130,34 @@ class Solution:
                 answer.append(last)
         
         return answer
+
+    
+# A clean and elegant solution. Only goes through the array once.    
+# 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+
+class Solution:
+    """
+    Insert a new interval into a sorted non-overlapping interval list.
+    @param intevals: Sorted non-overlapping interval list
+    @param newInterval: The new interval.
+    @return: A new sorted non-overlapping interval list with the new interval.
+    """
+    def insert(self, intervals, newInterval):
+        results = []
+        insertPos = 0
+        for interval in intervals:
+            if interval.end < newInterval.start:
+                results.append(interval)
+                insertPos += 1
+            elif interval.start > newInterval.end:
+                results.append(interval)
+            else:
+                newInterval.start = min(interval.start, newInterval.start)
+                newInterval.end = max(interval.end, newInterval.end)
+        results.insert(insertPos, newInterval)
+        return results
