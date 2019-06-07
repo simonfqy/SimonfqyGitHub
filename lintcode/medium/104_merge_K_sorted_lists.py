19a08ae13507cd45ed5b71ff567aca080dd14bc2
Qingyuan Feng <simonfengqy@gmail.com>
2019-06-07 14:06:07 -0700

    Update 104_merge_K_sorted_lists.py

--- lintcode/medium/104_merge_K_sorted_lists.py
+++ lintcode/medium/104_merge_K_sorted_lists.py
@@ -96,6 +96,53 @@ class Solution:
         return head
     
     
+    
+# 本参考程序来自九章算法，由 @令狐冲 提供。版权所有，转发请注明出处。
+# - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+# - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+# - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+# - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+
+class Solution:
+    """
+    @param lists: a list of ListNode
+    @return: The head of one sorted list.
+    """
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        
+        return self.merge_range_lists(lists, 0, len(lists) - 1)
+        
+    def merge_range_lists(self, lists, start, end):
+        if start == end:
+            return lists[start]
+        
+        mid = (start + end) // 2
+        left = self.merge_range_lists(lists, start, mid)
+        right = self.merge_range_lists(lists, mid + 1, end)
+        return self.merge_two_lists(left, right)
+        
+    # This way of merging using dummy node is more succinct than my code.
+    def merge_two_lists(self, head1, head2):
+        tail = dummy = ListNode(0)
+        while head1 and head2:
+            if head1.val < head2.val:
+                tail.next = head1
+                head1 = head1.next
+            else:
+                tail.next = head2
+                head2 = head2.next
+            tail = tail.next
+            
+        if head1:
+            tail.next = head1
+        if head2:
+            tail.next = head2
+        
+        return dummy.next
+    
+    
 # Bottom-up iterative approach.    
 class Solution:
     """
