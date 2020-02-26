8f40e9b3080e0b2cd09a431440e1fb2319be65f7
Qingyuan Feng <simonfengqy@gmail.com>
2020-02-25 22:45:05 -0800

    Update 902_Kth_smallest_element_in_a_bst.py

--- lintcode/medium/902_Kth_smallest_element_in_a_bst.py
+++ lintcode/medium/902_Kth_smallest_element_in_a_bst.py
@@ -63,3 +63,44 @@ class Solution:
                 node = node.right
                 
         return None
+    
+# Using the Java example from Jiuzhang.com. Divide-and-conquer, similar to quickSelect algorithm. Performs well for 
+# repeated reads.
+class Solution:
+    """
+    @param root: the given BST
+    @param k: the given k
+    @return: the kth smallest element in BST
+    """
+    def kthSmallest(self, root, k):
+        # write your code here
+        if root is None or k <= 0:
+            return None
+        subtree_element_count = dict()
+        self.get_element_counts(root, subtree_element_count)
+        return self.get_kth_smallest(root, k, subtree_element_count)
+        
+    def get_element_counts(self, root, subtree_element_count):
+        if root is None:
+            return 0
+        left_count = self.get_element_counts(root.left, subtree_element_count)
+        right_count = self.get_element_counts(root.right, subtree_element_count)
+        total_count = left_count + right_count + 1
+        subtree_element_count[root] = total_count
+        return total_count
+        
+    def get_kth_smallest(self, root, k, subtree_element_count):
+        if k <= 0 or k > subtree_element_count[root]:
+            return None
+        if root.left:
+            left_count = subtree_element_count[root.left]
+        else:
+            left_count = 0
+        if left_count >= k:
+            return self.get_kth_smallest(root.left, k, subtree_element_count)
+        if k == left_count + 1:
+            return root.val
+        # k > left_count + 1
+        return self.get_kth_smallest(root.right, k - left_count - 1, subtree_element_count)
+
+
