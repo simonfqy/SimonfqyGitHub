c6e963ad0e31749f5c9d14b0df461b5989f5f1a1
Qingyuan Feng <simonfengqy@gmail.com>
2020-03-14 23:31:14 -0700

    Update 95_validate_binary_search_tree.py

--- lintcode/medium/95_validate_binary_search_tree.py
+++ lintcode/medium/95_validate_binary_search_tree.py
@@ -89,8 +89,7 @@ class Solution:
             max_val = right_max
         else:
             max_val = root_val
-        return True, min_val, max_val
-    
+        return True, min_val, max_val    
 
 # Divide and conquer, but also uses a flag isBST to record the state while traversing the tree.
 # 本参考程序来自九章算法，由 @九章算法 提供。版权所有，转发请注明出处。
@@ -98,7 +97,6 @@ class Solution:
 # - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
 # - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
 # - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
-
 
 class Solution:
     """
@@ -120,3 +118,34 @@ class Solution:
             return
         self.lastVal = root.val
         self.validate(root.right)
+  
+# My own solution after 9 months. It is actually similar to the solution provided in jiuzhang's video lecture,
+# but more concise.
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+    def isValidBST(self, root):
+        # write your code here
+        is_bst, _, _ = self.check_validity(root)
+        return is_bst
+    
+    def check_validity(self, root):
+        if root is None:
+            return True, None, None
+            
+        left_is_bst, left_min, left_max = self.check_validity(root.left)
+        right_is_bst, right_min, right_max = self.check_validity(root.right)
+        if not left_is_bst or not right_is_bst:
+            return False, None, None
+        if left_max is not None and left_max >= root.val:
+            return False, None, None
+        if right_min is not None and right_min <= root.val:
+            return False, None, None
+        min_val, max_val = root.val, root.val
+        if left_min is not None:
+            min_val = left_min
+        if right_max is not None:
+            max_val = right_max
+        return True, min_val, max_val 
