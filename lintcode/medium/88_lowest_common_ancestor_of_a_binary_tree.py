b9951cfca7994b1055324ecce36fbd7e9e36f5b2
Qingyuan Feng <simonfengqy@gmail.com>
2020-03-29 14:38:25 -0700

    Update 88_lowest_common_ancestor_of_a_binary_tree.py

--- lintcode/medium/88_lowest_common_ancestor_of_a_binary_tree.py
+++ lintcode/medium/88_lowest_common_ancestor_of_a_binary_tree.py
@@ -45,3 +45,44 @@ class Solution:
         if (self.get_path(root.left, target)):
             return True
         return self.get_path(root.right, target)
+    
+# The iterative version corresponding to the recursive solution. Slightly better but takes more time.
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: A: A TreeNode in a Binary.
+    @param: B: A TreeNode in a Binary.
+    @return: Return the lowest common ancestor(LCA) of the two nodes.
+    """
+    def lowestCommonAncestor(self, root, A, B):
+        # write your code here
+        path_A = self.get_path(root, A)
+        path_B = self.get_path(root, B)
+        lca_candidate = root
+        for i in range(min(len(path_A), len(path_B))):
+            if path_A[i] == path_B[i]:
+                lca_candidate = path_A[i]
+            else:
+                break
+        return lca_candidate
+            
+    def get_path(self, root, target):
+        path = []
+        if root is None:
+            return path
+        stack = [root]
+        while len(stack) > 0:
+            node = stack.pop()
+            while len(path) > 0:
+                last_node = path[-1]
+                if last_node.left != node and last_node.right != node:
+                    path.pop()
+                else:
+                    break
+            path.append(node)
+            if node.val == target.val:
+                return path
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
