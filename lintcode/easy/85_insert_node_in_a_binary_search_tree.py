32bc66702cfeba5490aead88b7dedf1072cf33ce
Qingyuan Feng <simonfengqy@gmail.com>
2021-08-27 00:44:05 -0700

    Update 85_insert_node_in_a_binary_search_tree.py

--- lintcode/easy/85_insert_node_in_a_binary_search_tree.py
+++ lintcode/easy/85_insert_node_in_a_binary_search_tree.py
@@ -7,7 +7,7 @@ class TreeNode:
         self.left, self.right = None, None
 """
 
-
+# Straightforward recursive solution.
 class Solution:
     """
     @param: root: The root of the binary search tree.
@@ -22,4 +22,30 @@ class Solution:
             root.right = self.insertNode(root.right, node)
         if root.val > node.val:
             root.left = self.insertNode(root.left, node)
+        return root
+    
+    
+# A variation of the solution in jiuzhang.com. Does not use recursion.
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: node: insert this node into the binary search tree
+    @return: The root of the new binary search tree.
+    """
+    def insertNode(self, root, node):
+        # write your code here        
+        if not root:
+            return node
+        currt = root
+        while currt:
+            if currt.val > node.val:                
+                if not currt.left:
+                    currt.left = node
+                    break
+                currt = currt.left
+            else:
+                if not currt.right:
+                    currt.right = node
+                    break
+                currt = currt.right
         return root
