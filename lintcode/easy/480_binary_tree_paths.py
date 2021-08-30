7d408b14fabf74b3a477ef55c1d991812fe07994
Qingyuan Feng <simonfengqy@gmail.com>
2021-08-29 23:46:29 -0700

    Update 480_binary_tree_paths.py

--- lintcode/easy/480_binary_tree_paths.py
+++ lintcode/easy/480_binary_tree_paths.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.val = val
         self.left, self.right = None, None
 """
+
 # My own solution, uses recursion.
 class Solution:
     """
@@ -31,3 +32,33 @@ class Solution:
         if len(paths_to_return) == 0:
             return [path_prefix]
         return paths_to_return
+    
+    
+# This is using BFS, in which each entry stores a node and its corresponding path prefix. This is based
+# on a student's answer in jiuzhang.com.
+from collections import deque
+class Solution:
+    """
+    @param root: the root of the binary tree
+    @return: all root-to-leaf paths
+    """
+    def binaryTreePaths(self, root):
+        # write your code here
+        if not root:
+            return []
+        queue = deque([(root, '')])        
+        all_paths = []
+        while queue:
+            node, path_str = queue.pop()
+            if path_str != '':
+                path_str += "->"
+            path_str += str(node.val)
+            if node.left or node.right:
+                # Not a leaf node.                
+                if node.left:
+                    queue.appendleft((node.left, path_str))  
+                if node.right:
+                    queue.appendleft((node.right, path_str))              
+            else:
+                all_paths.append(path_str)
+        return all_paths
