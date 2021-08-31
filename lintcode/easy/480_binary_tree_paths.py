4e14fb2bec0473322424f4903f8874cb662044b2
Qingyuan Feng <simonfengqy@gmail.com>
2021-08-30 23:59:42 -0700

    Update 480_binary_tree_paths.py

--- lintcode/easy/480_binary_tree_paths.py
+++ lintcode/easy/480_binary_tree_paths.py
@@ -91,3 +91,25 @@ class Solution:
         if root.right:
             self.dfs(root.right, path + [str(root.right.val)], all_paths)
             
+
+# This an official answer in jiuzhang.com. It is kind of like post-order traversal.
+class Solution:
+    """
+    @param root: the root of the binary tree
+    @return: all root-to-leaf paths
+    """
+    def binaryTreePaths(self, root):
+        # write your code here
+        if not root:
+            return []
+        # 99% 的DFS题，不用单独处理叶子节点
+        # 这里需要单独处理的原因是, root为None的结果没有办法用于构造 root 是叶子的结果
+        if not root.left and not root.right:
+            return [str(root.val)]
+        all_paths = []
+        left_paths = self.binaryTreePaths(root.left)
+        right_paths = self.binaryTreePaths(root.right)           
+        for path in left_paths + right_paths:
+            # The path string is constructed from bottom up, all the way to root.
+            all_paths.append(str(root.val) + '->' + path)
+        return all_paths
