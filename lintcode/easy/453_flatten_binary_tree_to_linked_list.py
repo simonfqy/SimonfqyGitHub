d2d3ca7200fd4454e237e1e3595bfa7dcf6c7d26
Qingyuan Feng <simonfengqy@gmail.com>
2021-08-31 14:32:42 -0700

    Update 453_flatten_binary_tree_to_linked_list.py

--- lintcode/easy/453_flatten_binary_tree_to_linked_list.py
+++ lintcode/easy/453_flatten_binary_tree_to_linked_list.py
@@ -50,3 +50,25 @@ class Solution:
                 stack.append(node.right)
             if node.left:
                 stack.append(node.left)
+                
+                
+# Recursive solution provided by jiuzhang.com. Uses a global variable last_node. 
+class Solution:
+    last_node = None
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        # write your code here
+        if not root:
+            return
+        if self.last_node:
+            self.last_node.left = None
+            self.last_node.right = root
+        self.last_node = root
+        # This assignment is required. Otherwise, when self.flatten(root.left) is called, the root.right is modified 
+        # because root is the last_node in that function. So we need to keep a copy here.
+        right = root.right
+        self.flatten(root.left)        
+        self.flatten(right) 
