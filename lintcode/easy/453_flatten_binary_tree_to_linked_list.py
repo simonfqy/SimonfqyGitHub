24797c4a1deef5b78001b80e1c61c8c04d614366
Qingyuan Feng <simonfengqy@gmail.com>
2021-08-31 15:05:34 -0700

    Update 453_flatten_binary_tree_to_linked_list.py

--- lintcode/easy/453_flatten_binary_tree_to_linked_list.py
+++ lintcode/easy/453_flatten_binary_tree_to_linked_list.py
@@ -72,3 +72,26 @@ class Solution:
         right = root.right
         self.flatten(root.left)        
         self.flatten(right) 
+        
+        
+# Another recursive solution provided by jiuzhang.com. It uses a helper function to flatten the tree and return the last node. 
+class Solution:    
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        # write your code here
+        self.flatten_and_get_last_node(root)
+
+    def flatten_and_get_last_node(self, root):
+        if not root:
+            return None
+        left_last = self.flatten_and_get_last_node(root.left)
+        right_last = self.flatten_and_get_last_node(root.right)
+        if left_last:
+            left_last.right = root.right
+            root.right = root.left
+            root.left = None       
+        # This is a compact statement which returns the first non-null entity. 
+        return right_last or left_last or root 
