9ced585437406b54c9fae830866d09aae89bcd37
Qingyuan Feng <simonfengqy@gmail.com>
2021-08-31 17:48:58 -0700

    Update 93_balanced_binary_tree.py

--- lintcode/easy/93_balanced_binary_tree.py
+++ lintcode/easy/93_balanced_binary_tree.py
@@ -27,3 +27,29 @@ class Solution:
         if not right_balanced:
             return (False, -1)
         return (abs(left_depth - right_depth) <= 1, max(left_depth, right_depth) + 1)
+    
+    
+# My own solution. Uses a helper function to obtain depth.
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if this Binary tree is Balanced, or false.
+    """
+    def isBalanced(self, root):
+        # write your code here
+        if not root:
+            return True
+        left_balanced = self.isBalanced(root.left)
+        right_balanced = self.isBalanced(root.right)
+        if not (left_balanced and right_balanced):
+            return False
+        left_depth = self.get_depth(root.left)
+        right_depth = self.get_depth(root.right)
+        return abs(left_depth - right_depth) <= 1
+
+    def get_depth(self, root):
+        if not root:
+            return 0
+        left_depth = self.get_depth(root.left)
+        right_depth = self.get_depth(root.right)
+        return max(left_depth, right_depth) + 1
