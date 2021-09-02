0da641a905821ebdf21fe495fb0520c23d651afb
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-01 17:22:13 -0700

    Update 578_lowest_common_ancestor_iii.py

--- lintcode/medium/578_lowest_common_ancestor_iii.py
+++ lintcode/medium/578_lowest_common_ancestor_iii.py
@@ -35,4 +35,31 @@ class Solution:
             self.lca = root
             return True, True
         return have_a, have_b
+    
       
+# My own solution. Slightly modified from the previous one, uses recursion without global variables.
+class Solution:
+    """
+    @param: root: The root of the binary tree.
+    @param: A: A TreeNode
+    @param: B: A TreeNode
+    @return: Return the LCA of the two nodes.
+    """
+    def lowestCommonAncestor3(self, root, A, B):
+        # write your code here        
+        lca, _, _ = self.get_lca(root, A, B)
+        return lca
+
+    # return: lca, have_a, have_b
+    def get_lca(self, root, A, B):
+        if not root:
+            return None, False, False    
+        left_lca, left_has_a, left_has_b = self.get_lca(root.left, A, B)
+        right_lca, right_has_a, right_has_b = self.get_lca(root.right, A, B)
+        if left_lca or right_lca:
+            return left_lca or right_lca, True, True
+        have_a = left_has_a or right_has_a or root == A
+        have_b = left_has_b or right_has_b or root == B
+        if have_a and have_b:            
+            return root, True, True
+        return None, have_a, have_b
