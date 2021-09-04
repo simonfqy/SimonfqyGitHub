4dd0043f8491eee377bf89ee4ffad0c912b72bf6
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-03 19:21:30 -0700

    Update 90_k_sum_ii.py

--- lintcode/medium/90_k_sum_ii.py
+++ lintcode/medium/90_k_sum_ii.py
@@ -23,3 +23,29 @@ class Solution:
                 if complement:
                     combinations.append([curr_val] + complement)
         return combinations
+    
+    
+# Another recursive solution from jiuzhang.com. Very similar to my solution above.
+# It uses a semi-global variable "subsets" to contain all the satisfying results.
+class Solution:
+    """
+    @param: A: an integer array
+    @param: k: a postive integer <= length(A)
+    @param: targer: an integer
+    @return: A list of lists of integer
+    """
+    def kSumII(self, A, k, target):
+        # write your code here
+        subsets = []
+        self.dfs(A, 0, k, target, [], subsets)
+        return subsets
+
+    def dfs(self, A, index, k, target, subset, subsets):
+        if k == 0 and target == 0:
+            subsets.append(subset)
+            return
+        if k == 0 or target <= 0:
+            return
+        for i in range(index, len(A)):
+            curr_subset = subset + [A[i]]            
+            self.dfs(A, i + 1, k - 1, target - A[i], curr_subset, subsets)
