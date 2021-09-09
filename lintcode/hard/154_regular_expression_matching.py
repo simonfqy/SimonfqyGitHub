68c82202c206a9225d36b497554f8627392a5cdd
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-08 20:15:36 -0700

    Update 154_regular_expression_matching.py

--- lintcode/hard/154_regular_expression_matching.py
+++ lintcode/hard/154_regular_expression_matching.py
@@ -54,7 +54,9 @@ class Solution:
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if p[j - 1] == "*":
-                    dp[i][j] = dp[i][j - 2] or dp[i][j - 1] or (dp[i - 1][j] and (p[j - 2] == "." or p[j - 2] == s[i - 1]))
+                    # dp[i][j - 2] corresponds to the situation where "*" corresponds to 0 occurrence, while the body after "or" corresponds to
+                    # "*" matching 1 or more occurrences of the pattern before it.
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (p[j - 2] == "." or p[j - 2] == s[i - 1]))
                 else:
                     # p[j - 1] is "." or alphabet.
                     dp[i][j] = dp[i - 1][j - 1] and (p[j - 1] == "." or s[i - 1] == p[j - 1])                    
