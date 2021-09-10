2cbaa8d7548fb885e11fe530cf707223adfb5770
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-09 18:27:14 -0700

    Update 582_word_break_ii.py

--- lintcode/hard/582_word_break_ii.py
+++ lintcode/hard/582_word_break_ii.py
@@ -39,3 +39,36 @@ class Solution:
                         new_sentences.append(s[i : end + 1] + " " + continue_sentence)
         self.all_valid_words[(i, j)] = new_sentences
         return new_sentences
+    
+    
+# My own solution, slightly modified from the previous one. Still uses memorized DFS, and the 
+# dynamic programming table is removed.
+class Solution:
+    """
+    @param: s: A string
+    @param: wordDict: A set of words.
+    @return: All possible sentences.
+    """
+    def wordBreak(self, s, wordDict):
+        # write your code here
+        n = len(s)
+        # Represents whether the s[i: j + 1] is a valid word in wordDict.        
+        self.all_valid_words = dict()
+        return self.get_all_valid_words(s, 0, n - 1, wordDict)
+
+    # Return a list of valid words.
+    def get_all_valid_words(self, s, i, j, wordDict):
+        if (i, j) in self.all_valid_words:
+            return self.all_valid_words[(i, j)]
+        new_sentences = []
+        for end in range(i, j + 1):
+            substring = s[i : end + 1]
+            if substring in wordDict:                
+                if end == j:
+                    new_sentences.append(substring)
+                else:
+                    continuing_sentences = self.get_all_valid_words(s, end + 1, j, wordDict)
+                    for continue_sentence in continuing_sentences:
+                        new_sentences.append(substring + " " + continue_sentence)
+        self.all_valid_words[(i, j)] = new_sentences
+        return new_sentences
