7c916f4447b3b0012516170136b16f6744367722
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-15 20:14:22 -0700

    Update 153_combination_sum_ii.py

--- lintcode/medium/153_combination_sum_ii.py
+++ lintcode/medium/153_combination_sum_ii.py
@@ -28,3 +28,37 @@ class Solution:
                 # break instead of continue, because the num list is sorted and the next element will not be smaller.
                 break
             self.get_combinations(num, i + 1, end_ind, target, combination_so_far + [this_num])
+            
+            
+# My own solution. Does not use global variables. Instead of using DFS traversal, now it is divide-and-conquer,
+# and the helper function returns result.
+class Solution:
+    """
+    @param num: Given the candidate numbers
+    @param target: Given the target number
+    @return: All the combinations that sum to target
+    """
+    def combinationSum2(self, num, target):
+        # write your code here
+        num = sorted(num)
+        n = len(num)
+        return self.get_combinations(num, 0, n - 1, target)
+
+    def get_combinations(self, num, start_ind, end_ind, target):
+        if target == 0:
+            return [[]]   
+        combinations = []      
+        for i in range(start_ind, end_ind + 1):
+            this_num = num[i]
+            if this_num > target:
+                # break instead of continue, because the num list is sorted and the next element will not be smaller.
+                break                
+            suffix_combinations = self.get_combinations(num, i + 1, end_ind, target - this_num)
+            if not suffix_combinations:
+                continue
+            for suffix_combo in suffix_combinations:
+                complete_combo = [this_num] + suffix_combo
+                if complete_combo in combinations:
+                    continue
+                combinations.append(complete_combo)
+        return combinations
