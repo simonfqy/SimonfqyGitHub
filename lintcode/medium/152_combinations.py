d843896272857c55bbecbdd5ac8808e6a3a5da8e
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-17 01:04:54 -0700

    Update 152_combinations.py

--- lintcode/medium/152_combinations.py
+++ lintcode/medium/152_combinations.py
@@ -46,3 +46,29 @@ class Solution:
             for combo in suffix_combos:
                 combinations.append([i] + combo)
         return combinations
+
+# Slightly improved upon the previous solution by adding memoization.
+class Solution:
+    """
+    @param n: Given the range of numbers
+    @param k: Given the numbers of combinations
+    @return: All the combinations of k numbers out of 1..n
+    """
+    def combine(self, n, k):
+        # write your code here
+        start = 1
+        self.memo = dict()
+        return self.get_combinations(start, n, k)
+
+    def get_combinations(self, start, n, k):        
+        if k == 0:            
+            return [[]]
+        if (start, k) in self.memo:
+            return self.memo[(start, k)]
+        combinations = []
+        for i in range(start, n + 1):
+            suffix_combos = self.get_combinations(i + 1, n, k - 1)
+            for combo in suffix_combos:
+                combinations.append([i] + combo)
+        self.memo[(start, k)] = combinations
+        return combinations
