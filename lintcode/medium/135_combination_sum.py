0ae8bcdcc971cab13a3e5ff6be407c9529c62860
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-17 22:42:15 -0700

    Update 135_combination_sum.py

--- lintcode/medium/135_combination_sum.py
+++ lintcode/medium/135_combination_sum.py
@@ -28,3 +28,35 @@ class Solution:
             if i > start and curr_num == candidates[i - 1]:
                 continue
             self.helper(candidates, i, end, target - curr_num, combo_so_far + [curr_num], combos)
+            
+            
+# My own solution, recursive DFS with memoization, divide-and-conquer.
+class Solution:
+    """
+    @param candidates: A list of integers
+    @param target: An integer
+    @return: A list of lists of integers
+    """
+    def combinationSum(self, candidates, target):
+        # write your code here
+        candidates.sort()
+        self.memo = dict()
+        return self.helper(candidates, 0, len(candidates) - 1, target)
+
+    def helper(self, candidates, start, end, target):
+        if target == 0:            
+            return [[]]
+        if (start, target) in self.memo:
+            return self.memo[(start, target)]
+        combinations = []
+        for i in range(start, end + 1):
+            curr_num = candidates[i]
+            if curr_num > target:
+                break
+            if i > start and curr_num == candidates[i - 1]:
+                continue
+            suffix_combos = self.helper(candidates, i, end, target - curr_num)
+            for combo in suffix_combos:
+                combinations.append([curr_num] + combo)
+        self.memo[(start, target)] = combinations
+        return combinations
