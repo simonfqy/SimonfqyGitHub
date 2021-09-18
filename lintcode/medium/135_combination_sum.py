174f867c6e4b56c487d177088a0dbee065305a34
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-17 20:25:15 -0700

    Create 135_combination_sum.py

---
+++ lintcode/medium/135_combination_sum.py
@@ -0,0 +1,29 @@
+'''
+https://www.lintcode.com/problem/135/
+'''
+
+# My own solution, recursive DFS.
+class Solution:
+    """
+    @param candidates: A list of integers
+    @param target: An integer
+    @return: A list of lists of integers
+    """
+    def combinationSum(self, candidates, target):
+        # write your code here
+        candidates.sort()
+        combos = []
+        self.helper(candidates, 0, len(candidates) - 1, target, [], combos)
+        return combos
+
+    def helper(self, candidates, start, end, target, combo_so_far, combos):
+        if target == 0:
+            combos.append(combo_so_far)
+            return
+        for i in range(start, end + 1):
+            curr_num = candidates[i]
+            if curr_num > target:
+                break
+            if i > start and curr_num == candidates[i - 1]:
+                continue
+            self.helper(candidates, i, end, target - curr_num, combo_so_far + [curr_num], combos)
