50d9168b8710fb68b2fc78f9331221d808e1b86a
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-17 23:12:20 -0700

    Update 135_combination_sum.py

--- lintcode/medium/135_combination_sum.py
+++ lintcode/medium/135_combination_sum.py
@@ -60,3 +60,34 @@ class Solution:
                 combinations.append([curr_num] + combo)
         self.memo[(start, target)] = combinations
         return combinations
+    
+    
+# Solution from jiuzhang.com. Different from but similar to my own solution. 
+class Solution:
+    """
+    @param candidates: A list of integers
+    @param target: An integer
+    @return: A list of lists of integers
+    """
+    def combinationSum(self, candidates, target):
+        # write your code here
+        candidates.sort()
+        results = []
+        self.helper(candidates, 0, target, results, [])
+        return results
+    
+    def helper(self, candidates, start, target, results, combo_so_far):
+        if target == 0:
+            results.append(combo_so_far)
+            return
+        if start >= len(candidates) or target < 0:
+            return
+        
+        # Do not select the current start element.
+        self.helper(candidates, start + 1, target, results, combo_so_far)
+
+        if start > 0 and candidates[start] == candidates[start - 1]:
+            return
+
+        # Select the current start element
+        self.helper(candidates, start, target - candidates[start], results, combo_so_far + [candidates[start]])
