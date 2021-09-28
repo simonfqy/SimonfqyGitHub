2e20c3460afa20ba3953be284cb1ebd1245c1f21
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-27 23:58:13 -0700

    Update 198_permutation_index_ii.py

--- lintcode/medium/198_permutation_index_ii.py
+++ lintcode/medium/198_permutation_index_ii.py
@@ -2,7 +2,7 @@
 https://www.lintcode.com/problem/198/
 '''
 
-# My own solution. Took quite some effort to debug it.
+# My own solution. Iterative, starting from the end. Took quite some effort to debug it.
 from collections import defaultdict
 import math
 class Solution:
@@ -20,6 +20,7 @@ class Solution:
             permutation_count *= len(A) - 1 - i
             divisor = 1
             encountered_num = set()
+            # At first I placed this increment statement at the end of the outer loop and caused bugs.
             d[A[i]] += 1
             for j in range(i + 1, len(A)):
                 if A[j] < A[i]:
@@ -29,4 +30,32 @@ class Solution:
                     encountered_num.add(A[j])
             order += smaller_count * permutation_count / divisor            
 
+        return int(order)
+    
+# Also my own solution, essentially the same as the one above. Slightly more time intensive, because we calculate the factorial
+# for each i, instead of multiplying the permutation_count by (len(A) - 1 - i) for each i.
+from collections import defaultdict
+import math
+class Solution:
+    """
+    @param A: An array of integers
+    @return: A long integer
+    """
+    def permutationIndexII(self, A):
+        num_to_occurrence = defaultdict(int)
+        for num in A:
+            num_to_occurrence[num] += 1
+        order = 1
+        for i in range(len(A) - 1):
+            encountered_num = set()
+            smaller_count = 0
+            order_multiplier = math.factorial(len(A) - 1 - i)
+            for j in range(i + 1, len(A)):
+                if A[i] > A[j]:
+                    smaller_count += 1
+                if A[j] not in encountered_num:
+                    encountered_num.add(A[j])
+                    order_multiplier /= math.factorial(num_to_occurrence[A[j]])
+            order += smaller_count * order_multiplier
+            num_to_occurrence[A[i]] -= 1
         return int(order)
