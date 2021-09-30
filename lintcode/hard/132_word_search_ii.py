7bc604b49f00d8bf964d025c4543ffd928bdd38d
Qingyuan Feng <simonfengqy@gmail.com>
2021-09-30 00:36:40 -0700

    Update 132_word_search_ii.py

--- lintcode/hard/132_word_search_ii.py
+++ lintcode/hard/132_word_search_ii.py
@@ -25,7 +25,9 @@ class Solution:
         return existing_words
     
     # The board[start_i][start_j] character is not yet added to the end of the word_so_far argument. We'll see whether it matches and then
-    # add it if there is a match. Afterwards, we start from (start_i, start_j) to examine its neighbors.
+    # add it to word_so_far string if there is a match. Afterwards, we start from (start_i, start_j) to examine its neighbors.
+    # This logic is not as natural as the solution below, where word_so_far + board[pos[0]][pos[1]] is passed as the word_so_far parameter 
+    # when invoking the is_word_exist() function recursively on neighbors.
     def is_word_exist(self, board, word, word_so_far, start_i, start_j, visited):
         next_char = word[len(word_so_far)]
         if board[start_i][start_j] != next_char:
