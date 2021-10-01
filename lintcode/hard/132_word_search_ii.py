e56f98827efc45af9c629b61753041049e4e61f6
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-01 00:39:50 -0700

    Update 132_word_search_ii.py

--- lintcode/hard/132_word_search_ii.py
+++ lintcode/hard/132_word_search_ii.py
@@ -164,3 +164,73 @@ class Solution:
             
     def inside(self, board, x, y):
         return 0 <= x < len(board) and 0 <= y < len(board[0])
+    
+# Solution from jiuzhang.com. It uses trie. Each trie node contains some children and an indication of whether 
+# it corresponds to the last letter of a word.
+DIRECTIONS = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+class TrieNode:
+    def __init__(self):
+        self.is_word = False
+        self.word = None
+        self.children = dict()
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        node = self.root
+        for c in word:
+            child = node.children.get(c)
+            if not child:
+                node.children[c] = TrieNode()
+                child = node.children[c]
+            node = child
+        node.word = word
+        node.is_word = True
+    
+    # This function is actually not used, but added here for completeness of Trie functions.
+    def find(self, word):
+        node = self.root
+        for c in word:
+            node = node.children.get(c)
+            if not node:
+                return None
+        return node
+
+class Solution:
+    """
+    @param board: A list of lists of character
+    @param words: A list of string
+    @return: A list of string
+    """
+    def wordSearchII(self, board, words):
+        results = set()
+        trie = Trie()
+        for word in words:
+            trie.add(word)
+        
+        # We cannot formulate it in the same way as the 2 solutions I came up with.
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                c = board[i][j]
+                self.search(board, i, j, trie.root.children.get(c), results, set())
+        return list(results)    
+    
+    def search(self, board, x, y, node, results, visited):
+        if not node:
+            return
+        if node.is_word:
+            results.add(node.word)
+            # Don't just return here, because the current node could have descendants.
+
+        new_visited = set(visited)
+        new_visited.add((x, y))
+        for delta_x, delta_y in DIRECTIONS:
+            x_, y_ = x + delta_x, y + delta_y
+            if not (0 <= x_ < len(board) and 0 <= y_ < len(board[0])):
+                continue
+            if (x_, y_) in new_visited:
+                continue            
+            self.search(board, x_, y_, node.children.get(board[x_][y_]), results, new_visited)
+
