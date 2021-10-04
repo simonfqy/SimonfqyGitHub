eedc8864add7d0c4b520961fb9250ccf61edba82
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-03 19:53:07 -0700

    Update 425_letter_combinations_of_a_phone_number.py

--- lintcode/medium/425_letter_combinations_of_a_phone_number.py
+++ lintcode/medium/425_letter_combinations_of_a_phone_number.py
@@ -30,3 +30,39 @@ class Solution:
             return
         for char in self.digit_to_letter[digits[0]]:
             self.helper(digits[1:], combo_so_far + char, results)
+            
+            
+# Using iterative BFS solution.            
+from collections import deque
+class Solution:
+    """
+    @param digits: A digital string
+    @return: all posible letter combinations
+    """
+    def letterCombinations(self, digits):
+        # Have to handle this special case in the beginning.
+        if digits == "":
+            return []
+        results = []
+        self.digit_to_letter = dict()
+        self.digit_to_letter['2'] = ['a', 'b', 'c']
+        self.digit_to_letter['3'] = ['d', 'e', 'f']
+        self.digit_to_letter['4'] = ['g', 'h', 'i']
+        self.digit_to_letter['5'] = ['j', 'k', 'l']
+        self.digit_to_letter['6'] = ['m', 'n', 'o']
+        self.digit_to_letter['7'] = ['p', 'q', 'r', 's']
+        self.digit_to_letter['8'] = ['t', 'u', 'v']
+        self.digit_to_letter['9'] = ['w', 'x', 'y', 'z']
+        queue = deque([""])
+        curr_ind = 0
+        while queue:
+            if curr_ind == len(digits):
+                return list(queue)
+            # We're adding the characters corresponding to digits[curr_ind] in each iteration of the while loop.
+            # So we must process all combinations ending in digits[curr_ind - 1] before incrementing the curr_ind.
+            size = len(queue)
+            for _ in range(size):
+                combo = queue.popleft()            
+                for char in self.digit_to_letter[digits[curr_ind]]:
+                    queue.append(combo + char)
+            curr_ind += 1
