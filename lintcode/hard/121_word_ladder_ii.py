45e6bd0fa9d88918c49ff2158f950802d4ea8557
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-07 15:59:58 -0700

    Update 121_word_ladder_ii.py

--- lintcode/hard/121_word_ladder_ii.py
+++ lintcode/hard/121_word_ladder_ii.py
@@ -49,3 +49,54 @@ class Solution:
                 total_distance += 1                
         self.pair_distance[(from_word, to_word)] = total_distance
         return total_distance
+    
+    
+# My own solution, using BFS without queue, should be correct, but causes memory limit exceeded exception.
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: a list of lists of string
+    """
+    def findLadders(self, start, end, dictionary):
+        min_length_found = False
+        temp = [[start]]
+        self.word_dist = dict()
+        visited_words = set([start])
+        results = []
+        while not min_length_found:
+            buff = []
+            newly_visited = set()
+            # Every time the execution enters this for loop, the length of the (under-construction) word ladder
+            # is the same. So all lists in temp have the same length.
+            for i in range(len(temp)):
+                curr_word = temp[i][-1]
+                if self.calc_word_dist(curr_word, end) == 1:
+                    results.append(temp[i] + [end])
+                    min_length_found = True
+                if min_length_found:
+                    continue
+                for word in dictionary:
+                    # If the current word is visited by some other branches earlier (when the word ladder is shorter),
+                    # there's no point in diving into it any further; the current branch is guaranteed to be non-optimal.
+                    if word in visited_words:
+                        continue
+                    if self.calc_word_dist(curr_word, word) == 1:
+                        buff.append(temp[i] + [word])
+                        newly_visited.add(word)
+            visited_words.update(newly_visited)
+            temp = buff
+        return results
+    
+    def calc_word_dist(self, from_word, to_word):
+        if (from_word, to_word) in self.word_dist:
+            return self.word_dist[(from_word, to_word)]
+        if (to_word, from_word) in self.word_dist:
+            return self.word_dist[(to_word, from_word)]
+        distance = 0
+        for i in range(len(from_word)):
+            if from_word[i] != to_word[i]:
+                distance += 1                
+        self.word_dist[(from_word, to_word)] = distance
+        return distance
