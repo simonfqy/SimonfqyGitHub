9473f655b4079249dbc613554848453622daf537
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-06 19:58:22 -0700

    Update 425_letter_combinations_of_a_phone_number.py

--- lintcode/medium/425_letter_combinations_of_a_phone_number.py
+++ lintcode/medium/425_letter_combinations_of_a_phone_number.py
@@ -31,7 +31,36 @@ class Solution:
         for char in self.digit_to_letter[digits[0]]:
             self.helper(digits[1:], combo_so_far + char, results)
             
-            
+# Iterative DFS solution, using stack.            
+class Solution:
+    """
+    @param digits: A digital string
+    @return: all posible letter combinations
+    """
+    def letterCombinations(self, digits):
+        if digits == "":
+            return []
+        results = []
+        self.digit_to_letter = dict()
+        self.digit_to_letter['2'] = ['a', 'b', 'c']
+        self.digit_to_letter['3'] = ['d', 'e', 'f']
+        self.digit_to_letter['4'] = ['g', 'h', 'i']
+        self.digit_to_letter['5'] = ['j', 'k', 'l']
+        self.digit_to_letter['6'] = ['m', 'n', 'o']
+        self.digit_to_letter['7'] = ['p', 'q', 'r', 's']
+        self.digit_to_letter['8'] = ['t', 'u', 'v']
+        self.digit_to_letter['9'] = ['w', 'x', 'y', 'z']
+        stack = [""]
+        while stack:
+            combo = stack.pop()
+            if len(combo) == len(digits):
+                results.append(combo)
+                continue
+            for char in self.digit_to_letter[digits[len(combo)]]:
+                stack.append(combo + char)
+        return results
+    
+    
 # Using iterative BFS solution.            
 from collections import deque
 class Solution:
