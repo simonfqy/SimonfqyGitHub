92c70a502b8c64cd874038052b3d313ac6c499d4
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-11 14:54:13 -0700

    Update 120_word_ladder.py

--- lintcode/hard/120_word_ladder.py
+++ lintcode/hard/120_word_ladder.py
@@ -40,8 +40,7 @@ class Solution:
                     # populated fast enough, many words are unnecessarily visited more than once. So populate the 
                     # visited set early can avoid this problem. Always remember.
                     visited.add(word)                    
-        return 0
-        
+        return 0        
         
     def get_next_words(self, word):
         words = []
@@ -52,3 +51,67 @@ class Solution:
                     continue
                 words.append(left + char + right)
         return words
+    
+    
+# Bidirectional BFS solution from jiuzhang.com.    
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: An integer
+    """
+    def ladderLength(self, start, end, wordSet):
+        if start == end:
+            return 1
+        
+        wordSet.add(start)
+        wordSet.add(end)
+        graph = self.construct_graph(wordSet)
+        
+        forward_queue = collections.deque([start])
+        forward_set = set([start])
+        backward_queue = collections.deque([end])
+        backward_set = set([end])
+
+        distance = 1
+        while forward_queue and backward_queue:
+            distance += 1
+            if self.extend_queue(graph, forward_queue, forward_set, backward_set):
+                return distance
+            distance += 1
+            if self.extend_queue(graph, backward_queue, backward_set, forward_set):
+                return distance        
+        return -1
+        
+    def extend_queue(self, graph, queue, visited, opposite_visited):
+        for _ in range(len(queue)):
+            word = queue.popleft()
+            for next_word in graph[word]:
+                if next_word in visited:
+                    continue
+                if next_word in opposite_visited:
+                    return True
+                queue.append(next_word)
+                visited.add(next_word)
+        return False
+    
+    # This function is very simple, don't overcomplicate it by using queues. Implementing it with queue is problematic.      
+    def construct_graph(self, wordSet):
+        graph = {}
+        for word in wordSet:
+            graph[word] = self.get_next_words(word, wordSet)
+        return graph
+    
+    def get_next_words(self, word, wordSet):
+        next_word_set = set()
+        for i in range(len(word)):
+            prefix = word[:i]
+            suffix = word[i + 1:]
+            chars = list('abcdefghijklmnopqrstuvwxyz')
+            chars.remove(word[i])
+            for char in chars:
+                next_word = prefix + char + suffix
+                if next_word in wordSet:
+                    next_word_set.add(next_word)
+        return next_word_set
