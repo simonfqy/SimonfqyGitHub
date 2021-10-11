ceccd9080b2c2eb3a77a2163deb1f9da44917a6f
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-11 15:35:53 -0700

    Update 120_word_ladder.py

--- lintcode/hard/120_word_ladder.py
+++ lintcode/hard/120_word_ladder.py
@@ -53,6 +53,45 @@ class Solution:
         return words
     
     
+# Solution from jiuzhang.com, it doesn't use level order traversal (层次遍历).     
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: An integer
+    """
+    def ladderLength(self, start, end, dict):
+        dict.add(end)
+        queue = collections.deque([start])
+        distance = {start: 1}
+            
+        while queue:
+            word = queue.popleft()
+            if word == end:
+                return distance[word]
+            
+            for next_word in self.get_next_words(word, dict):
+                if next_word in distance:
+                    continue
+                queue.append(next_word)
+                distance[next_word] = distance[word] + 1
+
+        return 0
+        
+    def get_next_words(self, word, dict):
+        words = []
+        for i in range(len(word)):
+            left, right = word[:i], word[i + 1:]
+            for char in 'abcdefghijklmnopqrstuvwxyz':
+                if word[i] == char:
+                    continue
+                next_word = left + char + right
+                if next_word in dict:
+                    words.append(next_word)
+
+        return words    
+    
 # Bidirectional BFS solution from jiuzhang.com.    
 class Solution:
     """
