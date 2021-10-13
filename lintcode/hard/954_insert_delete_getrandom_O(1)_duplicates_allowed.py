cf9bcd130065fbe1680deb6dcc3b6d776c8314ec
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-12 22:51:25 -0700

    Update 954_insert_delete_getrandom_O(1)_duplicates_allowed.py

--- lintcode/hard/954_insert_delete_getrandom_O(1)_duplicates_allowed.py
+++ lintcode/hard/954_insert_delete_getrandom_O(1)_duplicates_allowed.py
@@ -1,5 +1,6 @@
 '''
 https://www.lintcode.com/problem/954/
+Leetcode link: https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/
 '''
 
 # My own solution. Similar to question 657:
@@ -114,3 +115,63 @@ class RandomizedCollection(object):
         :rtype: int
         """
         return random.choice(self.values)
+    
+    
+# The solution below is for leetcode. It is from jiuzhang.com, and I translated it from Java to Python. It is rather complicated and
+# hard to think of. Took me tremendous effort to understand and translate it.
+class RandomizedCollection:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.val_to_index_list = dict()
+        self.values_and_indices = []        
+
+    def insert(self, val):
+        """
+        Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        return_bool = val not in self.val_to_index_list
+        if return_bool:
+            self.val_to_index_list[val] = []
+        # Each tuple stored in self.values_and_indices is the value and the order number of the current value among all its occurrences.
+        # For example, we can have a list with values [4, 2, 4, 4], for which self.values_and_indices would be [(4, 0), (2, 0), (4, 1), (4, 2)].
+        # In the last tuple (4, 2), 4 is the value, while 2 means it is the 2nd 4 (the order starts from 0). The corresponding self.val_to_index_list[4]
+        # is [0, 2, 3]. 
+        # So how is it used? In the remove() function, if we are removing 4 from a list of values [4, 2, 4, 1, 1], originally self.val_to_index_list[1]
+        # is [3, 4], now it becomes [3, 2], since we move the last element to the position where we want the element removed. The ending list becomes
+        # [4, 2, 1, 1], where the first 1 with index 2 is actually the original second 1. The original first 1 is unchanged, we only move the second
+        # 1 to the front.
+        self.values_and_indices.append((val, len(self.val_to_index_list[val])))
+        self.val_to_index_list[val].append(len(self.values_and_indices) - 1)        
+        return return_bool        
+
+    def remove(self, val):
+        """
+        Removes a value from the collection. Returns true if the collection contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.val_to_index_list:
+            return False
+        index_list_of_removing_element = self.val_to_index_list[val]
+        last_ind_of_removing_element = index_list_of_removing_element[-1]
+        # Copy the last element to the position to substitute the one to be removed.
+        last_val_and_index = self.values_and_indices[-1]
+        self.values_and_indices[last_ind_of_removing_element] = last_val_and_index        
+        self.val_to_index_list[last_val_and_index[0]][last_val_and_index[1]] = last_ind_of_removing_element
+        self.val_to_index_list[val].pop()
+        if not self.val_to_index_list[val]:
+            del self.val_to_index_list[val]     
+        self.values_and_indices.pop()        
+        return True         
+
+    def getRandom(self):
+        """
+        Get a random element from the collection.
+        :rtype: int
+        """
+        return random.choice(self.values_and_indices)[0]
