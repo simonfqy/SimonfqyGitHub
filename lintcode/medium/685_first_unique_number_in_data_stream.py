156782ef697a5c9a4165c2f1ee3ee152cfa3f131
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-12 23:45:10 -0700

    Create 685_first_unique_number_in_data_stream.py

---
+++ lintcode/medium/685_first_unique_number_in_data_stream.py
@@ -0,0 +1,35 @@
+'''
+https://www.lintcode.com/problem/685/
+'''
+
+# My own solution. The efficiency is not great, but much better than my initial version which maintains a list of candidate unique
+# numbers. Whenever we do a remove() operation the time complexity is O(n). Now with dictionary, the del time complexity is O(1).
+class Solution:
+    """
+    @param nums: a continuous stream of numbers
+    @param number: a number
+    @return: returns the first unique number
+    """
+    def firstUniqueNumber(self, nums, number):
+        terminating_number_found = False
+        candidate_unique_numbers_to_ind = dict()
+        ind_to_candidate_unique_numbers = dict()
+        repeated_numbers = set()
+        for i, num in enumerate(nums):            
+            if num in repeated_numbers:
+                continue
+            if num in candidate_unique_numbers_to_ind:
+                index = candidate_unique_numbers_to_ind[num]
+                del candidate_unique_numbers_to_ind[num]
+                del ind_to_candidate_unique_numbers[index]
+                repeated_numbers.add(num)
+                continue
+            candidate_unique_numbers_to_ind[num] = i
+            ind_to_candidate_unique_numbers[i] = num
+            if num == number:
+                terminating_number_found = True
+                break
+        
+        if not terminating_number_found:
+            return -1
+        return ind_to_candidate_unique_numbers[min(ind_to_candidate_unique_numbers.keys())]
