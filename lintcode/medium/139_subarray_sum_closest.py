7d31808fd45ab398c7345307e9973037970c6eff
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-17 12:50:57 -0700

    Update 139_subarray_sum_closest.py

--- lintcode/medium/139_subarray_sum_closest.py
+++ lintcode/medium/139_subarray_sum_closest.py
@@ -158,3 +158,27 @@ class Solution:
             prev_value = prefix_sum
             
         return subarray_inds
+    
+    
+# Answer from Lintcode official. It is similar to my own solution but not as optimized, so it has less lines of code.    
+class Solution:
+    """
+    @param: nums: A list of integers
+    @return: A list of integers includes the index of the first number and the index of the last number
+    """
+    def subarraySumClosest(self, nums):
+        prefix_sum = [(0, -1)]
+        for i, num in enumerate(nums):
+            prefix_sum.append((prefix_sum[-1][0] + num, i))
+            
+        prefix_sum.sort()
+        
+        closest, answer = sys.maxsize, []
+        for i in range(1, len(prefix_sum)):
+            if closest > prefix_sum[i][0] - prefix_sum[i - 1][0]:
+                closest = prefix_sum[i][0] - prefix_sum[i - 1][0]
+                left = min(prefix_sum[i - 1][1], prefix_sum[i][1]) + 1
+                right = max(prefix_sum[i - 1][1], prefix_sum[i][1])
+                answer = [left, right]
+        
+        return answer
