73c3eff18f7c11d5b8653c52f9f807cb20508d17
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-18 16:09:18 -0700

    Update 171_anagrams.py

--- lintcode/medium/171_anagrams.py
+++ lintcode/medium/171_anagrams.py
@@ -21,3 +21,27 @@ class Solution:
             if len(sorted_word_to_original_words[sorted_word]) > 1:
                 results.extend(sorted_word_to_original_words[sorted_word])
         return results
+    
+    
+# My own solution. Slightly modified based on the solution above. Constructs the return list while traversing through the
+# strs, does not need to visit the words twice.
+class Solution:
+    """
+    @param strs: A list of strings
+    @return: A list of strings
+    """
+    def anagrams(self, strs):
+        results = []
+        sorted_word_to_original_words = dict()
+        sorted_word_visited = set()
+        for word in strs:
+            sorted_word = ''.join(sorted(list(word)))
+            if sorted_word not in sorted_word_to_original_words:
+                sorted_word_to_original_words[sorted_word] = [word]
+            else:
+                if sorted_word not in sorted_word_visited:
+                    results += sorted_word_to_original_words[sorted_word]
+                    sorted_word_visited.add(sorted_word)
+                results.append(word)
+
+        return results
