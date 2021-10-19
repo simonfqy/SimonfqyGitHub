e83834ff73d82f0b1e7a9507a647d0210d120feb
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-19 13:54:41 -0700

    Update 372_delete_node_in_a_linked_list.py

--- lintcode/easy/372_delete_node_in_a_linked_list.py
+++ lintcode/easy/372_delete_node_in_a_linked_list.py
@@ -28,3 +28,20 @@ class Solution:
             # Previously I wrote it to be curr_node = next_node, it resulted in errors.
             curr_node = curr_node.next
             next_node = next_node.next
+            
+            
+# My solution, optimized from the one above. It's actually very simple. We just need to replicate the current node to the next node, 
+# then change the pointer to the second next node of the current node, then we're done.  
+class Solution:
+    """
+    @param: node: the node in the list should be deleted
+    @return: nothing
+    """
+    def deleteNode(self, node):
+        if not node:
+            return
+        next_node = node.next
+        curr_node = node
+        if next_node:
+            curr_node.val = next_node.val
+            curr_node.next = next_node.next
