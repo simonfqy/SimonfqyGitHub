5d6036d07f7e42c48bda43c45110fdf91bf1496a
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-20 13:26:54 -0700

    Update 81_find_median_from_data_stream.py

--- lintcode/hard/81_find_median_from_data_stream.py
+++ lintcode/hard/81_find_median_from_data_stream.py
@@ -34,3 +34,41 @@ class Solution:
     """
     def getMedian(self) -> int:
         return self.data[(len(self.data) - 1) // 2]
+    
+    
+# This solution uses heap and should be correct, but also causes time limit exceeded exception.
+import heapq
+class Solution:
+    def __init__(self):
+        self.smaller_half_negative_heap, self.bigger_half_heap = [], []
+        heapq.heapify(self.smaller_half_negative_heap)
+        heapq.heapify(self.bigger_half_heap)
+
+    """
+    @param val: a num from the data stream.
+    @return: nothing
+    """
+    def add(self, val: int):  
+        if len(self.smaller_half_negative_heap) > len(self.bigger_half_heap):
+            biggest_element_in_smaller_half = -heapq.nsmallest(1, self.smaller_half_negative_heap)[0]
+            # We need to increment the self.bigger_half_heap.
+            if val >= biggest_element_in_smaller_half:
+                heapq.heappush(self.bigger_half_heap, val)
+            else:
+                largest_in_smaller_half = -heapq.heapreplace(self.smaller_half_negative_heap, -val)
+                heapq.heappush(self.bigger_half_heap, largest_in_smaller_half)
+        else:      
+            if len(self.bigger_half_heap) > 0:
+                smallest_element_in_larger_half = heapq.nsmallest(1, self.bigger_half_heap)[0]
+            # Increment the self.smaller_half_negative_heap
+            if len(self.smaller_half_negative_heap) == 0 or val <= smallest_element_in_larger_half:
+                heapq.heappush(self.smaller_half_negative_heap, -val)
+            else:
+                smallest_in_larger_half = heapq.heappushpop(self.bigger_half_heap, val)
+                heapq.heappush(self.smaller_half_negative_heap, -smallest_in_larger_half)
+
+    """
+    @return: return the median of the all numbers
+    """
+    def getMedian(self) -> int:
+        return -heapq.nsmallest(1, self.smaller_half_negative_heap)[0]
