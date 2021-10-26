34576f324f22bc1db455d14a0ef10f2b7b786d61
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-26 00:49:29 -0700

    Create 544_top_k_largest_numbers.py

---
+++ lintcode/medium/544_top_k_largest_numbers.py
@@ -0,0 +1,21 @@
+'''
+https://www.lintcode.com/problem/544
+'''
+
+# My own solution. Uses heap. Time complexity should be O(nlogn), where n is the length of nums.
+import heapq
+class Solution:
+    """
+    @param nums: an integer array
+    @param k: An integer
+    @return: the top k largest numbers in array
+    """
+    def topk(self, nums, k):
+        negative_nums = []
+        for num in nums:
+            heapq.heappush(negative_nums, -num)
+        results = []
+        while len(results) < k:
+            results.append(-heapq.heappop(negative_nums))
+        return results
+            
