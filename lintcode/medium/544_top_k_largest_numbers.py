7a421387eb3aa5f9526750576e0ed1076a16f15d
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-26 14:07:11 -0700

    Update 544_top_k_largest_numbers.py

--- lintcode/medium/544_top_k_largest_numbers.py
+++ lintcode/medium/544_top_k_largest_numbers.py
@@ -34,3 +34,37 @@ class Solution:
         top_k.reverse()
         return top_k
             
+        
+# Slightly converted from the solution provided by students of jiuzhang.com. Uses quickSelect algorithm to make nums[:k]
+# contain the k largest elements of nums array. Then sort that sublist reversely (in descending order) and return it.
+class Solution:
+    """
+    @param nums: an integer array
+    @param k: An integer
+    @return: the top k largest numbers in array
+    """
+    def topk(self, nums, k):
+        self.quick_select(nums, 0, len(nums) - 1, k - 1)
+        top_k = nums[:k]
+        top_k.sort(reverse=True)
+        return top_k
+
+    def quick_select(self, nums, start, end, ind):
+        if start == end:
+            return
+        left, right = start, end
+        pivot = nums[(left + right) // 2]
+        while left <= right:
+            while left <= right and nums[left] > pivot:
+                left += 1
+            while left <= right and nums[right] < pivot:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left, right = left + 1, right - 1
+        if left <= ind:
+            self.quick_select(nums, left, end, ind)
+            return
+        if right >= ind:
+            self.quick_select(nums, start, right, ind)
+            
