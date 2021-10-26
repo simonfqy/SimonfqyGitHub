e772c9a90eb5b5b27f595374e1fb02820f1906ea
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-26 00:53:37 -0700

    Update 544_top_k_largest_numbers.py

--- lintcode/medium/544_top_k_largest_numbers.py
+++ lintcode/medium/544_top_k_largest_numbers.py
@@ -18,4 +18,19 @@ class Solution:
         while len(results) < k:
             results.append(-heapq.heappop(negative_nums))
         return results
+    
+            
+# My own, simple solution using Python sort function.
+import heapq
+class Solution:
+    """
+    @param nums: an integer array
+    @param k: An integer
+    @return: the top k largest numbers in array
+    """
+    def topk(self, nums, k):
+        nums.sort()
+        top_k = nums[len(nums) - k:]
+        top_k.reverse()
+        return top_k
             
