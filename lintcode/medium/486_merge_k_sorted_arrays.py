13b78cd74ec3791a039e0d2b3a354ac94e4b4805
Qingyuan Feng <simonfengqy@gmail.com>
2021-10-27 13:03:57 -0700

    Update 486_merge_k_sorted_arrays.py

--- lintcode/medium/486_merge_k_sorted_arrays.py
+++ lintcode/medium/486_merge_k_sorted_arrays.py
@@ -32,6 +32,32 @@ class Solution:
         return answer
     
     
+# The original solution from jiuzhang.com (similar to the one above, but more succinct). It stores 3-tuple in heap to store (number, index in big array, 
+# index of the element in the small array), so we don't need to keep a separate array/dictionary for storing the indices of the current elements in each small array.
+import heapq
+class Solution:
+    """
+    @param arrays: k sorted integer arrays
+    @return: a sorted array
+    """
+    def mergekSortedArrays(self, arrays):
+        result = []
+        heap = []
+        for index, array in enumerate(arrays):
+            if len(array) == 0:
+                continue
+            heapq.heappush(heap, (array[0], index, 0))
+             
+        while len(heap):
+            val, x, y = heap[0]
+            heapq.heappop(heap)
+            result.append(val)
+            if y + 1 < len(arrays[x]):
+                heapq.heappush(heap, (arrays[x][y + 1], x, y + 1))
+            
+        return result
+    
+    
 # A recursive approach, top-down.
 class Solution:
     """
