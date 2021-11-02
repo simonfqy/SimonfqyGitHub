a2d4a000091b7c234fd88f56eabd3274ba65cdc3
Qingyuan Feng <simonfengqy@gmail.com>
2021-11-02 14:43:07 -0700

    Create 191_maximum_product_subarray.py

---
+++ lintcode/medium/191_maximum_product_subarray.py
@@ -0,0 +1,42 @@
+'''
+Link: https://www.lintcode.com/problem/191
+'''
+
+# My own solution. Took a VERY long time to come up with. Time complexity is O(n).
+class Solution:
+    """
+    @param nums: An array of integers
+    @return: An integer
+    """
+    def maxProduct(self, nums):
+        max_product = float('-inf')
+        min_product = float('inf')
+        # These numbers record the maximum and minimum products of subarrays ending at nums[right]. I only came to this clear realization
+        # after finishing the code. Next time should try to clarify it earlier.
+        candidate_max_product, candidate_min_product = 1, 1
+        for right in range(len(nums)):
+            new_num = nums[right]
+            if new_num > 0:                
+                if candidate_max_product > 0:
+                    candidate_max_product *= new_num
+                else:
+                    candidate_max_product = new_num                           
+                if candidate_min_product > 0:
+                    candidate_min_product = new_num
+                else:
+                    candidate_min_product *= new_num               
+                                
+            elif new_num < 0:
+                if candidate_max_product > 0:
+                    candidate_max_product, candidate_min_product = candidate_min_product * new_num, candidate_max_product * new_num
+                elif candidate_max_product < 0:
+                    candidate_max_product, candidate_min_product = candidate_min_product * new_num, new_num
+                else:
+                    candidate_max_product, candidate_min_product = candidate_min_product * new_num, new_num
+            else:
+                candidate_max_product, candidate_min_product = 0, 0    
+            min_product = min(min_product, candidate_min_product)
+            max_product = max(max_product, candidate_max_product)             
+                                                            
+        return max_product
+
