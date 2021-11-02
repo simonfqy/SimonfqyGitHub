a359191da4ace26a4e060fade412f025931f117e
Qingyuan Feng <simonfengqy@gmail.com>
2021-11-01 18:38:05 -0700

    Update 1726_word_subsets.py

--- lintcode/medium/1726_word_subsets.py
+++ lintcode/medium/1726_word_subsets.py
@@ -2,7 +2,9 @@
 https://www.lintcode.com/problem/1726
 '''
 
-# My own solution. Should be correct, but hits time limit exceeded exception.
+# My own solution. Originally hits time limit exceeded exception without removing duplicate b words. After the duplication
+# removal, the performance is pretty well. Assuming A has size n and B has size m, and words in A and B have max length k, then
+# the time complexity is O(mnk^2).
 class Solution:
     """
     @param A: a string array
@@ -14,17 +16,20 @@ class Solution:
         a_word_to_sorted_word = dict()
         for a_word in A:
             a_word_to_sorted_word[a_word] = ''.join(sorted(a_word))
+        visited_b = set()
         for b_word in B:
-            subset = self.get_subset(subset, a_word_to_sorted_word, b_word)
-            if not subset:
-                return []
+            # Before having this duplicate removal of b_words, it hit time limit exceeded exception.
+            sorted_b_word = ''.join(sorted(b_word))
+            if sorted_b_word in visited_b:
+                continue
+            subset = self.get_subset(subset, a_word_to_sorted_word, sorted_b_word, visited_b)     
+            visited_b.add(sorted_b_word)       
         return subset
     
-    def get_subset(self, subset, a_word_to_sorted_word, b_word):
+    def get_subset(self, subset, a_word_to_sorted_word, sorted_b_word, visited_b):
         new_subset = []
         for a_word in subset:
             sorted_a_word = a_word_to_sorted_word[a_word]
-            sorted_b_word = ''.join(sorted(b_word))
             if self.b_word_in_a_word(sorted_a_word, sorted_b_word):
                 new_subset.append(a_word)
         return new_subset
@@ -34,13 +39,11 @@ class Solution:
         for i in range(len(sorted_b_word)):
             char = sorted_b_word[i]
             if char == sorted_b_word[prev]:
-                # We can simply use a continue statement to replace the following if-else statements, but keep them here for clarity.
-                if i < len(sorted_b_word) - 1:
-                    continue
-                else:
-                    return sorted_b_word[prev:] in sorted_a_word
+                # There are actually two scenarios, one in which i == len(sorted_b_word) - 1, one with i smaller than that.
+                # But both will be okay with this "continue" statement.
+                continue
             if sorted_b_word[prev : i] not in sorted_a_word:
                 return False
             prev = i
-        return sorted_b_word[prev:] in sorted_a_word
+        return sorted_b_word[prev:] in sorted_a_word            
             
