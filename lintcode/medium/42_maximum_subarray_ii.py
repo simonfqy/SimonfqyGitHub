508cc962907ef564aacbe64201704e27f040d07d
Qingyuan Feng <simonfengqy@gmail.com>
2021-11-03 18:16:09 -0700

    Update 42_maximum_subarray_ii.py

--- lintcode/medium/42_maximum_subarray_ii.py
+++ lintcode/medium/42_maximum_subarray_ii.py
@@ -34,3 +34,34 @@ class Solution:
             max_subarrays_sum = max(max_subarrays_sum, left_max_sum[i] + right_max_sum[i + 1])
         return max_subarrays_sum
         
+        
+# Another of my own solution, likewise, basically copied from the solution of question 45:
+# https://github.com/simonfqy/SimonfqyGitHub/blob/6f70a9737255b06bb8192fad8db618de6c60426a/lintcode/medium/45_maximum_subarray_difference.py#L54
+# It uses dynamic programming and greedy algorithm. 
+class Solution:
+    """
+    @param: nums: A list of integers
+    @return: An integer denotes the sum of max two non-overlapping subarrays
+    """
+    def maxTwoSubArrays(self, nums):
+        if not nums or len(nums) <= 1:
+            return 0
+        n = len(nums)
+        max_subarray_sum_on_left, max_subarray_sum_on_right = [0] * n, [0] * n
+        sum_of_max_current_subarray = nums[0]
+        max_subarray_sum_on_left[0], max_subarray_sum_on_right[n - 1] = nums[0], nums[n - 1]
+        for i in range(1, n - 1):
+            sum_of_max_current_subarray = max(nums[i], sum_of_max_current_subarray + nums[i])
+            max_subarray_sum_on_left[i] = max(max_subarray_sum_on_left[i - 1], sum_of_max_current_subarray)
+        
+        sum_of_max_current_subarray = nums[n - 1]
+        for j in range(n - 2, -1, -1):
+            sum_of_max_current_subarray = max(nums[j], sum_of_max_current_subarray + nums[j])
+            max_subarray_sum_on_right[j] = max(max_subarray_sum_on_right[j + 1], sum_of_max_current_subarray)
+
+        max_subarray_sum = float('-inf')
+        for i in range(n - 1):
+            max_subarray_sum = max(max_subarray_sum, max_subarray_sum_on_left[i] + max_subarray_sum_on_right[i + 1])
+        return max_subarray_sum
+    
+    
