181d71369b583058f32e997958509a335aa5caf1
Qingyuan Feng <simonfengqy@gmail.com>
2021-12-09 02:53:37 -0800

    Update 721_next_sparse_number.py

--- lintcode/hard/721_next_sparse_number.py
+++ lintcode/hard/721_next_sparse_number.py
@@ -47,4 +47,46 @@ class Solution:
         for i in range(n - 1, -1, -1):
             result += int(binary[i]) << (n - 1 - i)
         return result
+    
       
+# My own solution, slightly optimized compared to the above solution. Removes the is_sparse() helper function.
+class Solution:
+    """
+    @param x: a number
+    @return: return the next sparse number behind x
+    """
+    def nextSparseNum(self, x):
+        x_binary = self.convert_to_binary(x)        
+        n = len(x_binary)
+        pos_rightmost_consecutive_zero = 0
+        is_sparse = True
+        for i in range(1, n):
+            if x_binary[i] == '1' and x_binary[i - 1] == '1':
+                is_sparse = False
+                break
+            if x_binary[i] == '0' and x_binary[i - 1] == '0':
+                pos_rightmost_consecutive_zero = i
+        if is_sparse:
+            return x
+        # Get the converted binary.
+        next_sparse = x_binary[:pos_rightmost_consecutive_zero] + '1' + '0' * (n - 1 - pos_rightmost_consecutive_zero)
+        return self.convert_to_decimal(next_sparse)
+    
+    # convert number to a binary string.
+    def convert_to_binary(self, num):
+        binary = ""
+        i = 0
+        while 2 ** i <= num:
+            binary = str((num >> i) & 1) + binary
+            i += 1
+        if not binary or binary[0] != "0": 
+            binary = "0" + binary
+        return binary    
+
+    def convert_to_decimal(self, binary):
+        result = 0
+        n = len(binary)
+        for i in range(n - 1, -1, -1):
+            result += int(binary[i]) << (n - 1 - i)
+        return result
+    
