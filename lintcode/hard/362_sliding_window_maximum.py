62864da625266d553f2d030e28a2ca2643f23349
Qingyuan Feng <simonfengqy@gmail.com>
2021-12-13 03:52:56 -0800

    Update 362_sliding_window_maximum.py

--- lintcode/hard/362_sliding_window_maximum.py
+++ lintcode/hard/362_sliding_window_maximum.py
@@ -18,11 +18,32 @@ class Solution:
         queue = deque()
         for right in range(n):
             if right >= k:
-                # Pop the earlier element(s).
+                # Pop the earlier element(s). Note that the only possible earlier element is nums[right - k], so we can reduce this 2-level indentation
+                # to 1-level, and make it an if statement rather than while loop. 
+                # Why elements before nums[right - k] will not appear in the front of the queue? Because if queue[0][0] is right - k - 1, it would have
+                # been popped when right == right - 1. How about queue[0][0] being right - k and queue[1][0] being right - k - 1? It is also impossible, 
+                # because it would have been popped when nums[right - k], which is larger than nums[right - k - 1], was added to the queue.
                 while queue and queue[0][0] <= right - k:
                     queue.popleft()
             # Add the right element. Pop the elements which are earlier and smaller, since they won't appear in the result anyways.
             # This popping operation will help maintain the order, making sure that the largest element is always at the left end of the queue.
+            while queue and queue[-1][1] <= nums[right]:
+                queue.pop()
+            queue.append((right, nums[right]))
+            if right >= k - 1:
+                results.append(queue[0][1])
+        return results
+    
+    # Slight variant with the de-que operation changed from while loop to if statement.
+    def maxSlidingWindow2(self, nums, k):
+        n = len(nums)
+        results = []
+        # Each element: (index, value)
+        queue = deque()
+        for right in range(n):
+            # Explained above. The only possible earlier element is nums[right - k] at this point.
+            if right >= k and queue and queue[0][0] <= right - k:
+                queue.popleft()            
             while queue and queue[-1][1] <= nums[right]:
                 queue.pop()
             queue.append((right, nums[right]))
