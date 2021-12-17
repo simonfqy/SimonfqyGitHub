cc08c7d71b919fcabf05788a798fbd3c5699197a
Qingyuan Feng <simonfengqy@gmail.com>
2021-12-17 05:09:22 -0800

    Create 151_best_time_to_buy_and_sell_stock_iii.py

---
+++ lintcode/medium/151_best_time_to_buy_and_sell_stock_iii.py
@@ -0,0 +1,42 @@
+'''
+Link: https://www.lintcode.com/problem/151
+'''
+
+# My own solution. Has O(n) time complexity. Traverses through the array twice.
+class Solution:
+    """
+    @param prices: Given an integer array
+    @return: Maximum profit
+    """
+    def maxProfit(self, prices):
+        if len(prices) < 2:
+            return 0
+        max_profit = 0
+        n = len(prices)
+        left_max_profits = [0] * n
+        right_max_profits = [0] * (n + 1)
+        self.get_maximum_profit_arrays(prices, left_max_profits, right_max_profits)
+        for i in range(n):            
+            max_profit = max(max_profit, left_max_profits[i] + right_max_profits[i + 1])
+        return max_profit
+    
+    def get_maximum_profit_arrays(self, prices_list, left_max_profits, right_max_profits):
+        n = len(prices_list)        
+        reverse_negative_prices_list = prices_list[::-1]
+        reverse_negative_prices_list = [-a for a in reverse_negative_prices_list]        
+        min_price = prices_list[0]
+        max_profit = 0            
+        for i in range(n):
+            price = prices_list[i]
+            max_profit = max(max_profit, price - min_price)
+            min_price = min(min_price, price)
+            left_max_profits[i] = max_profit
+        min_price = reverse_negative_prices_list[0]
+        max_profit = 0
+        for i in range(n):
+            price = reverse_negative_prices_list[i]
+            max_profit = max(max_profit, price - min_price)
+            min_price = min(min_price, price)
+            right_max_profits[n - i - 1] = max_profit
+        return 
+      
