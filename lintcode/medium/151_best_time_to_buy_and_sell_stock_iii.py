b50d69121ce3bd37dae1d65a42445afd7a4630bb
Qingyuan Feng <simonfengqy@gmail.com>
2021-12-20 06:54:56 -0800

    Update 151_best_time_to_buy_and_sell_stock_iii.py

--- lintcode/medium/151_best_time_to_buy_and_sell_stock_iii.py
+++ lintcode/medium/151_best_time_to_buy_and_sell_stock_iii.py
@@ -39,4 +39,47 @@ class Solution:
             min_price = min(min_price, price)
             right_max_profits[n - i - 1] = max_profit
         return 
+    
       
+# My own solution, refactored from the above solution by using helper functions to remove duplicate code.
+class Solution:
+    """
+    @param prices: Given an integer array
+    @return: Maximum profit
+    """
+    def maxProfit(self, prices):
+        if len(prices) < 2:
+            return 0
+        max_profit = 0
+        n = len(prices)
+        left_max_profits = [0] * n
+        right_max_profits = [0] * n
+        self.populate_maximum_profit_arrays(prices, left_max_profits, right_max_profits)
+        # Add a 0 to the end to satisfy the query of right_max_profits[n].
+        right_max_profits.append(0)
+        for i in range(n):            
+            max_profit = max(max_profit, left_max_profits[i] + right_max_profits[i + 1])
+        return max_profit
+    
+    def populate_maximum_profit_arrays(self, prices_list, left_max_profits, right_max_profits):
+        n = len(prices_list)        
+        reverse_negative_prices_list = prices_list[::-1]
+        reverse_negative_prices_list = [-a for a in reverse_negative_prices_list]        
+        self.populate_single_array(prices_list, left_max_profits)
+        self.populate_single_array(reverse_negative_prices_list, right_max_profits)
+        # We have to use right_max_profits.reverse() here, not right_max_profits = right_max_profits[::-1]. Only
+        # calling the reverse() function can actually change the underlying list referenced from the scope of 
+        # the parent function. 
+        right_max_profits.reverse()
+        return 
+
+    def populate_single_array(self, prices_list, max_profit_array):
+        min_price = prices_list[0]
+        max_profit = 0
+        for i in range(len(prices_list)):
+            curr_price = prices_list[i]
+            max_profit = max(max_profit, curr_price - min_price)
+            min_price = min(min_price, curr_price)
+            max_profit_array[i] = max_profit
+            
+            
