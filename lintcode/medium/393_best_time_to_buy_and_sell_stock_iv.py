70f27ddc0a82a72a2d6566a6b30a1c690785656c
Qingyuan Feng <simonfengqy@gmail.com>
2021-12-28 05:52:02 -0800

    Update 393_best_time_to_buy_and_sell_stock_iv.py

--- lintcode/medium/393_best_time_to_buy_and_sell_stock_iv.py
+++ lintcode/medium/393_best_time_to_buy_and_sell_stock_iv.py
@@ -34,3 +34,37 @@ class Solution:
                 dp[transaction_count][i] = max(dp[transaction_count][i - 1], max_profit_containing_curr_price)             
             
         return dp[K][n - 1]
+    
+    
+# Solution from jiuzhang.com. Has O(nk) time and space complexities.  
+class Solution:
+    """
+    @param K: An integer
+    @param prices: An integer array
+    @return: Maximum profit
+    """
+    def maxProfit(self, K, prices):
+        n = len(prices)
+        if n < 2 or K == 0:
+            return 0
+        # corner case: equal to infinite times of transaction
+        if K >= n // 2:
+            max_profit = 0
+            for i in range(1, n):
+                max_profit += max(0, prices[i] - prices[i - 1])
+            return max_profit
+        dp = [[0] * n for _ in range(K + 1)]
+        for transaction_count in range(1, K + 1):           
+            max_diff = float('-inf')    
+            for i in range(1, n):
+                # Maintaining a max_diff variable is actually an optimization from the prior version where the time complexity 
+                # is O(kn^2). For each i, we want to calculate a value using dp[transaction_count - 1][t] + prices[i] - prices[t],
+                # where t can be 0, 1, 2, ..., i - 1, we select the t which maximizes this expression. Using max_diff to record the
+                # maximum value of dp[transaction_count - 1][i - 1] - prices[i - 1] as we traverse through i = 1 to n - 1 can save
+                # time and avoid duplicate calculation.                 
+                max_diff = max(max_diff, dp[transaction_count - 1][i - 1] - prices[i - 1])
+                dp[transaction_count][i] = max(dp[transaction_count][i - 1], prices[i] + max_diff)             
+            
+        return dp[K][n - 1]
+    
+    
