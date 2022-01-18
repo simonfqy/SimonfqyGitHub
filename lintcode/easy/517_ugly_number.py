22ebc5f15141a731da477ae864b06f1364ad93a7
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-18 15:55:58 -0800

    Create 517_ugly_number.py

---
+++ lintcode/easy/517_ugly_number.py
@@ -0,0 +1,28 @@
+'''
+Link: https://www.lintcode.com/problem/517
+'''
+
+# My own solution. Not very elegant, has two special condition checks.
+class Solution:
+    """
+    @param num: An integer
+    @return: true if num is an ugly number or false
+    """
+    def isUgly(self, num: int) -> bool:
+        if num < 1:
+            return False
+        if num == 1:
+            return True
+        expected_prime_factors = [2, 3, 5]
+        divisible = True
+        while divisible:
+            divisible = False
+            for prime_factor in expected_prime_factors:
+                if num % prime_factor == 0:
+                    divisible = True
+                    num /= prime_factor
+                    break
+            
+        return num == 1
+      
+      
