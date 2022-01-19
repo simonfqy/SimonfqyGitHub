a69ecc6c1e54e6c7cb33cca2be4a4f95898696a4
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-18 19:04:32 -0800

    Create 518_super_ugly_number.py

---
+++ lintcode/medium/518_super_ugly_number.py
@@ -0,0 +1,26 @@
+'''
+Link: https://www.lintcode.com/problem/518
+'''
+
+# My own solution. Uses dynamic programming, has O(n) time and space complexities. This solution is basically copied from
+# https://github.com/simonfqy/SimonfqyGitHub/blob/d9370febac2ec7aac65edf564abaa5887f2c2e21/lintcode/medium/4_ugly_number_ii.py#L52
+class Solution:
+    """
+    @param n: a positive integer
+    @param primes: the given prime list
+    @return: the nth super ugly number
+    """
+    def nthSuperUglyNumber(self, n, primes):
+        k = len(primes)
+        pointers = [0] * k
+        dp = [1] * n
+        for i in range(1, n):
+            candidates = []
+            for j in range(k):
+                candidates.append(dp[pointers[j]] * primes[j])
+            dp[i] = min(candidates)
+            for j in range(k):
+                if candidates[j] == dp[i]:
+                    pointers[j] += 1
+
+        return dp[n - 1]
