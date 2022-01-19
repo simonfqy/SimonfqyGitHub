56770e9b8751c6bc69e7c47bc0dc8dd0a321695d
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-18 19:16:45 -0800

    Update 518_super_ugly_number.py

--- lintcode/medium/518_super_ugly_number.py
+++ lintcode/medium/518_super_ugly_number.py
@@ -26,3 +26,25 @@ class Solution:
         return dp[n - 1]
 
     
+# My own solution. Uses heap, has O(nklognk) time complexity and O(nk) space complexity. This solution is basically copied from
+# https://github.com/simonfqy/SimonfqyGitHub/blob/d9370febac2ec7aac65edf564abaa5887f2c2e21/lintcode/medium/4_ugly_number_ii.py#L33
+import heapq
+class Solution:
+    """
+    @param n: a positive integer
+    @param primes: the given prime list
+    @return: the nth super ugly number
+    """
+    def nthSuperUglyNumber(self, n, primes):
+        min_heap = [(1, 1)]
+        ugly_num = 1
+        for _ in range(n):
+            ugly_num, m = heapq.heappop(min_heap)
+            for factor in primes:
+                if factor < m:
+                    continue
+                heapq.heappush(min_heap, (ugly_num * factor, factor))
+        return ugly_num
+    
+
+    
