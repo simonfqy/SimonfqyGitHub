6b82ff21e133bcc28b338cde92456421c8299ba8
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-18 16:14:58 -0800

    Update 517_ugly_number.py

--- lintcode/easy/517_ugly_number.py
+++ lintcode/easy/517_ugly_number.py
@@ -24,3 +24,18 @@ class Solution:
         return num == 1
       
       
+# Solution modified from the one provided by jiuzhang.com. Much more elegant.
+class Solution:
+    """
+    @param num: An integer
+    @return: true if num is an ugly number or false
+    """
+    def isUgly(self, num: int) -> bool:
+        ugly_factors = [2, 3, 5]    
+        for factor in ugly_factors:   
+            while num >= factor and num % factor == 0:
+                num /= factor
+                    
+        return num == 1
+    
+    
