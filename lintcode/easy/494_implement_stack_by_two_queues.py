c216078f2be6103838a9c1d8fda04ed96c7505ee
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-22 00:29:16 -0800

    Update 494_implement_stack_by_two_queues.py

--- lintcode/easy/494_implement_stack_by_two_queues.py
+++ lintcode/easy/494_implement_stack_by_two_queues.py
@@ -63,3 +63,44 @@ class Stack:
     def isEmpty(self):
         # write your code here
         return len(self.queue_1) == 0 and len(self.queue_2) == 0
+
+   
+# My own solution. Require that current_queue always only contain 1 element, such that every time top() operation only has O(1)
+# time complexity, rather than the O(n) top() operation in the solution provided by jiuzhang.com.
+from collections import deque
+class Stack:
+    def __init__(self):
+        self.current_queue, self.majority_queue = deque(), deque()
+
+    """
+    @param: x: An integer
+    @return: nothing
+    """
+    def push(self, x):
+        if self.current_queue:
+            self.majority_queue.append(self.current_queue.popleft())
+        self.current_queue.append(x)
+
+    """
+    @return: nothing
+    """
+    def pop(self):
+        self.current_queue.popleft()
+        while len(self.majority_queue) > 1:
+            element = self.majority_queue.popleft()
+            self.current_queue.append(element)
+        self.current_queue, self.majority_queue = self.majority_queue, self.current_queue
+
+    """
+    @return: An integer
+    """
+    def top(self):
+        return self.current_queue[0]
+
+    """
+    @return: True if the stack is empty
+    """
+    def isEmpty(self):
+        return (len(self.current_queue) + len(self.majority_queue)) == 0
+    
+    
