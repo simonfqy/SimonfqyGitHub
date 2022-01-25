109360fa023b27cdb42638a69c64329cb3abf5d2
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-25 14:22:13 -0800

    Update 840_range_sum_query_mutable.py

--- lintcode/medium/840_range_sum_query_mutable.py
+++ lintcode/medium/840_range_sum_query_mutable.py
@@ -30,3 +30,36 @@ class NumArray:
         return sum(self.nums[i : j + 1])
       
       
+# Should be correct, but hits the time limit exceeded exception.
+class NumArray:
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+        self.prefix_sum = [0] * (len(nums) + 1)        
+        for i in range(len(self.nums)):
+            self.prefix_sum[i + 1] = self.prefix_sum[i] + nums[i]
+
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: void
+        """
+        diff = val - self.nums[i]
+        self.nums[i] = val
+        for j in range(i + 1, len(self.nums) + 1):
+            self.prefix_sum[j] += diff        
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.prefix_sum[j + 1] - self.prefix_sum[i]
+    
+    
+    
