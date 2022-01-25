5018943dc5dbdf2196f0a5833def6584219e5905
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-25 14:49:13 -0800

    Update 840_range_sum_query_mutable.py

--- lintcode/medium/840_range_sum_query_mutable.py
+++ lintcode/medium/840_range_sum_query_mutable.py
@@ -62,4 +62,49 @@ class NumArray:
         return self.prefix_sum[j + 1] - self.prefix_sum[i]
     
     
+# Also hits the time limit exceeded exception. The bad thing about this algorithm is that, sumRange() time complexity depends on the number
+# of update() operations executed.
+class NumArray:
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+        self.index_to_diff = dict()
+        self.prefix_sum = [0] * (len(nums) + 1)        
+        for i in range(len(self.nums)):
+            self.prefix_sum[i + 1] = self.prefix_sum[i] + nums[i]
+
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: void
+        """
+        aggregated_diff = val - self.nums[i]
+        self.nums[i] = val
+        if i in self.index_to_diff:
+            aggregated_diff = self.index_to_diff[i] + aggregated_diff
+            if aggregated_diff == 0:
+                del self.index_to_diff[i]
+                return
+        self.index_to_diff[i] = aggregated_diff
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        original_sum = self.prefix_sum[j + 1] - self.prefix_sum[i]
+        for ind, diff in sorted(self.index_to_diff.items()):
+            if ind < i:
+                continue
+            if ind > j:
+                break
+            original_sum += diff
+        return original_sum
+    
+    
     
