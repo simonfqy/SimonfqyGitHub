99ef43466e65d9b42adbaa46a9ecc9f21b86b7e8
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-25 13:18:11 -0800

    Update 665_range_sum_query_2d_immutable.py

--- lintcode/medium/665_range_sum_query_2d_immutable.py
+++ lintcode/medium/665_range_sum_query_2d_immutable.py
@@ -40,3 +40,33 @@ class NumMatrix:
         return bottom_right_sum - bottom_left_sum
       
       
+# Also my own solution, slightly simplified from the above version. Here we are padding the entries with 0s, which simplifies the sumRegion() function. 
+class NumMatrix:
+    """
+    @param: matrix: a 2D matrix
+    """
+    def __init__(self, matrix):
+        self.coord_to_prefix_sum = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+        prefix_sum_col = [0] * (len(matrix[0]) + 1)
+        for row in range(len(matrix)):
+            prefix_sum = 0
+            for col in range(len(matrix[0])):
+                prefix_sum_col[col + 1] += matrix[row][col]
+                prefix_sum += prefix_sum_col[col + 1]
+                self.coord_to_prefix_sum[row + 1][col + 1] = prefix_sum
+
+    """
+    @param: row1: An integer
+    @param: col1: An integer
+    @param: row2: An integer
+    @param: col2: An integer
+    @return: An integer
+    """
+    def sumRegion(self, row1, col1, row2, col2):
+        bottom_right_sum = self.coord_to_prefix_sum[row2 + 1][col2 + 1] - self.coord_to_prefix_sum[row1][col2 + 1]
+        bottom_left_sum = self.coord_to_prefix_sum[row2 + 1][col1] - self.coord_to_prefix_sum[row1][col1]                             
+
+        return bottom_right_sum - bottom_left_sum
+    
+    
+    
