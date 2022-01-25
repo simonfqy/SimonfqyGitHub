f466291c814bde3a1116360d8b0f3d285d1ad39d
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-24 19:24:45 -0800

    Create 548_intersection_of_two_arrays_ii.py

---
+++ lintcode/easy/548_intersection_of_two_arrays_ii.py
@@ -0,0 +1,31 @@
+'''
+Link: https://www.lintcode.com/problem/548/
+'''
+
+# My own solution. Uses dictionary to store the occurrence of each element.
+from collections import defaultdict
+class Solution:
+    """
+    @param nums1: an integer array
+    @param nums2: an integer array
+    @return: an integer array
+    """
+    def intersection(self, nums1, nums2):
+        nums1_element_to_freq, common_element_to_freq = defaultdict(int), dict()
+        for num in nums1:
+            nums1_element_to_freq[num] += 1
+        for num in nums2:
+            if nums1_element_to_freq[num] == 0:
+                continue
+            if num in common_element_to_freq and common_element_to_freq[num] == nums1_element_to_freq[num]:
+                continue
+            if num not in common_element_to_freq:
+                common_element_to_freq[num] = 0
+            common_element_to_freq[num] += 1
+        result = []
+        for num in common_element_to_freq:
+            occurrence = common_element_to_freq[num]
+            result += [num] * occurrence
+        return result
+      
+      
