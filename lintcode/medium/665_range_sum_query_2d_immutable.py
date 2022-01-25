75c8c7814da11620b98fdb47270d19b151624236
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-25 13:38:25 -0800

    Update 665_range_sum_query_2d_immutable.py

--- lintcode/medium/665_range_sum_query_2d_immutable.py
+++ lintcode/medium/665_range_sum_query_2d_immutable.py
@@ -69,4 +69,31 @@ class NumMatrix:
         return bottom_right_sum - bottom_left_sum
     
     
+# Solution from jiuzhang.com. It is similar to my own solution above, but it simplifies the calculation of entries in the dp matrix.  
+class NumMatrix:
+    """
+    @param: matrix: a 2D matrix
+    """
+    def __init__(self, matrix):
+        n, m = len(matrix), len(matrix[0])
+        self.dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for row in range(n):
+            for col in range(m):              
+                # This formula is more intuitive than the prefix sum calculation I used.  
+                self.dp[row + 1][col + 1] = self.dp[row][col + 1] + self.dp[row + 1][col] + matrix[row][col] - self.dp[row][col]
+
+    """
+    @param: row1: An integer
+    @param: col1: An integer
+    @param: row2: An integer
+    @param: col2: An integer
+    @return: An integer
+    """
+    def sumRegion(self, row1, col1, row2, col2):
+        bottom_right_sum = self.dp[row2 + 1][col2 + 1] - self.dp[row1][col2 + 1]
+        bottom_left_sum = self.dp[row2 + 1][col1] - self.dp[row1][col1]                             
+
+        return bottom_right_sum - bottom_left_sum   
+
+    
     
