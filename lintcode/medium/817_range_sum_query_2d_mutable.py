7b22739fe1856235e76577f4f2f779e4566a027f
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-29 01:02:31 -0800

    Update 817_range_sum_query_2d_mutable.py

--- lintcode/medium/817_range_sum_query_2d_mutable.py
+++ lintcode/medium/817_range_sum_query_2d_mutable.py
@@ -160,4 +160,97 @@ class NumMatrix(object):
                 self.range_sum_query(self.segtrees_for_each_row[row1], col1 + 1, col2 + 1, 0, self.m, 0)
     
     
+# My own solution, but inspired by jiuzhang.com on BIT trees. This one uses 2D segment tree. Should be correct, but unfortunately it 
+# hits time limit exceeded exception.
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        # n rows, m columns.
+        self.n, self.m = len(matrix), len(matrix[0])
+        self.matrix = [[0] * self.m for _ in range(self.n)]
+        self.segtree_size = self.get_segtree_size(self.n + 1)
+        self.row_segtree_size = self.get_segtree_size(self.m + 1)
+        self.segtrees = [[0] * (self.row_segtree_size) for _ in range(self.segtree_size)]
+        for row in range(self.n):            
+            for col in range(self.m):
+                self.update(row, col, matrix[row][col])            
+
+    def get_segtree_size(self, num_entries):
+        i = 0
+        segtree_size = 0
+        while True:
+            power = 2 ** i
+            if power >= num_entries:
+                segtree_size = 2 * power - 1
+                break
+            i += 1
+        return segtree_size    
+
+    def update(self, row, col, val):
+        """
+        :type row: int
+        :type col: int
+        :type val: int
+        :rtype: void
+        """     
+        diff = val - self.matrix[row][col]
+        self.matrix[row][col] = val
+        self.update_2d_segtree(0, self.n, 0, self.m, row + 1, col + 1, 0, 0, diff)                    
+
+    def update_2d_segtree(self, low_1, high_1, low_2, high_2, row_ind, col_ind, pos_1, pos_2, diff):
+        if low_1 > row_ind or high_1 < row_ind:
+            return
+        if low_1 == high_1 == row_ind:
+            self.update_1d_segtree(low_2, high_2, col_ind, pos_1, pos_2, diff)
+            return
+        mid_1 = (low_1 + high_1) // 2
+        self.update_2d_segtree(low_1, mid_1, low_2, high_2, row_ind, col_ind, 2 * pos_1 + 1, pos_2, diff)
+        self.update_2d_segtree(mid_1 + 1, high_1, low_2, high_2, row_ind, col_ind, 2 * pos_1 + 2, pos_2, diff)
+        # This calculation is elegant but not very efficient.
+        self.segtrees[pos_1] = [(x + y) for x, y in zip(self.segtrees[2 * pos_1 + 1], self.segtrees[2 * pos_1 + 2])]
+
+    def update_1d_segtree(self, low, high, ind, pos_1, pos_2, diff):
+        if low > ind or high < ind:
+            return
+        if low == high == ind:
+            self.segtrees[pos_1][pos_2] += diff
+            return
+        mid = (low + high) // 2
+        self.update_1d_segtree(low, mid, ind, pos_1, 2 * pos_2 + 1, diff)
+        self.update_1d_segtree(mid + 1, high, ind, pos_1, 2 * pos_2 + 2, diff)
+        self.segtrees[pos_1][pos_2] = self.segtrees[pos_1][2 * pos_2 + 1] + self.segtrees[pos_1][2 * pos_2 + 2]
+           
+    def range_sum_query_2d(self, qlow_1, qhigh_1, qlow_2, qhigh_2, low_1, high_1, low_2, high_2, pos_1, pos_2):
+        if low_1 > qhigh_1 or high_1 < qlow_1:
+            return 0
+        if low_1 >= qlow_1 and high_1 <= qhigh_1:
+            return self.range_sum_query_1d(qlow_2, qhigh_2, low_2, high_2, pos_1, pos_2)
+        mid_1 = (low_1 + high_1) // 2
+        left_range_sum = self.range_sum_query_2d(qlow_1, qhigh_1, qlow_2, qhigh_2, low_1, mid_1, low_2, high_2, 2 * pos_1 + 1, pos_2)
+        right_range_sum = self.range_sum_query_2d(qlow_1, qhigh_1, qlow_2, qhigh_2, mid_1 + 1, high_1, low_2, high_2, 2 * pos_1 + 2, pos_2)
+        return left_range_sum + right_range_sum
+
+    def range_sum_query_1d(self, qlow, qhigh, low, high, pos_1, pos_2):
+        if low > qhigh or high < qlow:
+            return 0
+        if low >= qlow and high <= qhigh:
+            return self.segtrees[pos_1][pos_2]
+        mid = (low + high) // 2
+        left_range_sum = self.range_sum_query_1d(qlow, qhigh, low, mid, pos_1, 2 * pos_2 + 1)
+        right_range_sum = self.range_sum_query_1d(qlow, qhigh, mid + 1, high, pos_1, 2 * pos_2 + 2)
+        return left_range_sum + right_range_sum
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        return self.range_sum_query_2d(row1 + 1, row2 + 1, col1 + 1, col2 + 1, 0, self.n, 0, self.m, 0, 0)  
+    
     
