dbdc8b18ebcf9b7693da1e2947ff6f57c10c4fb4
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-28 19:53:02 -0800

    Update 817_range_sum_query_2d_mutable.py

--- lintcode/medium/817_range_sum_query_2d_mutable.py
+++ lintcode/medium/817_range_sum_query_2d_mutable.py
@@ -3,7 +3,7 @@ Link: https://www.lintcode.com/problem/817/
 '''
 
 
-# Let each column be a binary indexed tree. We also have padded 0s as the first row and column, respectively.
+# My own solution. Let each column be a binary indexed tree. We also have padded 0s as the first row and column, respectively.
 # Assuming there are n rows and m columns. The update() operation has O(mlogn) time complexity, while sumRegion() has O(logn) time complexity.
 # The initialization has O(mnlogn) time complexity.
 class NumMatrix(object):
@@ -72,3 +72,89 @@ class NumMatrix(object):
         return bottom_right - bottom_left
       
       
+# My own solution. Let each row be a segment tree. We also have padded 0s as the first row and column, respectively.
+# Assuming there are n rows and m columns. The update() operation has O(nlogm) time complexity, while sumRegion() has O(logm) time complexity.
+# The initialization has O(nmlogm) time complexity.
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        # n rows, m columns.
+        self.n, self.m = len(matrix), len(matrix[0])
+        self.matrix = matrix
+        i = 0
+        self.segtree_size = 0
+        while True:
+            power = 2 ** i
+            if power >= self.m + 1:
+                self.segtree_size = 2 * power - 1
+                break
+            i += 1
+        self.segtrees_for_each_row = [[0] * (self.segtree_size) for _ in range(self.n + 1)]
+        for row_ind in range(self.n + 1):
+            if row_ind == 0:
+                row = [0] * (self.m + 1)
+            else:
+                prev_row = row
+                curr_row = [0] + matrix[row_ind - 1]
+                row = [(x + y) for x, y in zip(prev_row, curr_row)]
+            self.construct_segtree(row, self.segtrees_for_each_row[row_ind], 0, self.m, 0)
+
+    def construct_segtree(self, array, segtree, low, high, pos):       
+        if low == high:
+            segtree[pos] = array[low]
+            return
+        mid = (low + high) // 2
+        self.construct_segtree(array, segtree, low, mid, 2 * pos + 1)
+        self.construct_segtree(array, segtree, mid + 1, high, 2 * pos + 2)
+        segtree[pos] = segtree[2 * pos + 1] + segtree[2 * pos + 2]  
+
+    def update(self, row, col, val):
+        """
+        :type row: int
+        :type col: int
+        :type val: int
+        :rtype: void
+        """      
+        index = col + 1
+        diff = val - self.matrix[row][col]
+        self.matrix[row][col] = val
+        for row_ind in range(row + 1, self.n + 1):
+            self.update_segtree(self.segtrees_for_each_row[row_ind], index, diff, 0, self.m, 0)        
+
+    def update_segtree(self, segtree, index, diff, low, high, pos):
+        if low > index or high < index:
+            return
+        if low == high == index:
+            segtree[pos] += diff
+            return
+        mid = (low + high) // 2
+        self.update_segtree(segtree, index, diff, low, mid, 2 * pos + 1)
+        self.update_segtree(segtree, index, diff, mid + 1, high, 2 * pos + 2)
+        segtree[pos] = segtree[2 * pos + 1] + segtree[2 * pos + 2]
+           
+    def range_sum_query(self, segtree, qlow, qhigh, low, high, pos):
+        if low > qhigh or high < qlow:
+            return 0
+        if low >= qlow and high <= qhigh:
+            return segtree[pos]
+        mid = (low + high) // 2
+        left_range_sum = self.range_sum_query(segtree, qlow, qhigh, low, mid, 2 * pos + 1)
+        right_range_sum = self.range_sum_query(segtree, qlow, qhigh, mid + 1, high, 2 * pos + 2)
+        return left_range_sum + right_range_sum
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        return self.range_sum_query(self.segtrees_for_each_row[row2 + 1], col1 + 1, col2 + 1, 0, self.m, 0) - \
+                self.range_sum_query(self.segtrees_for_each_row[row1], col1 + 1, col2 + 1, 0, self.m, 0)
+    
+    
+    
