d5123206a07ce1bb6906a8e8e3d6d328dcc3895d
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-29 14:14:05 -0800

    Update 817_range_sum_query_2d_mutable.py

--- lintcode/medium/817_range_sum_query_2d_mutable.py
+++ lintcode/medium/817_range_sum_query_2d_mutable.py
@@ -344,3 +344,50 @@ class NumMatrix(object):
         return self.range_sum_query_2d(row1 + 1, row2 + 1, col1 + 1, col2 + 1, 0, self.n, 0, self.m, 0, 0)  
     
     
+# Another solution from a student on jiuzhang.com. Does not use advanced data structures. Each prefix_sum[i][j] is the sum of the jth element from
+# rows matrix[0] to matrix[i].    
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        self.matrix = matrix
+        self.prefix_sum = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i == 0:
+                    self.prefix_sum[i][j] = matrix[i][j]
+                else:
+                    self.prefix_sum[i][j] = matrix[i][j] + self.prefix_sum[i-1][j]
+            
+    def update(self, row, col, val):
+        """
+        :type row: int
+        :type col: int
+        :type val: int
+        :rtype: void
+        """
+        diff = val - self.matrix[row][col]
+        self.matrix[row][col] = val 
+        for i in range(row, len(self.matrix)):
+            self.prefix_sum[i][col] += diff
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        ans = 0
+        for j in range(col1, col2+1):
+            if row1 > 0:
+                ans += (self.prefix_sum[row2][j] - self.prefix_sum[row1-1][j])
+            else:
+                ans += self.prefix_sum[row2][j]
+
+        return ans
+    
+    
