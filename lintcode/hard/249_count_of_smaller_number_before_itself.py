cd2ad5f67880b0f6295f202ad3dc79693ed5dee3
Qingyuan Feng <simonfengqy@gmail.com>
2022-01-31 14:40:41 -0800

    Update 249_count_of_smaller_number_before_itself.py

--- lintcode/hard/249_count_of_smaller_number_before_itself.py
+++ lintcode/hard/249_count_of_smaller_number_before_itself.py
@@ -229,4 +229,61 @@ class Solution:
         return results
     
     
+# Solution from jiuzhang.com. Uses value-based segment tree, but here the segment tree is implemented in tree nodes, not array.
+class SegTree:
+    
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
+        self.count = 0
+        if start != end:
+            self.left = SegTree(start, (start + end) // 2)
+            self.right = SegTree((start + end) // 2 + 1, end)
+    
+    def sum(self, start, end):
+        if start <= self.start and end >= self.end:
+            return self.count
+
+        if end < self.start or start > self.end:
+            return 0
+        
+        if self.start == self.end:
+            return 0       
+                
+        return (self.left.sum(start, end) + 
+                self.right.sum(start, end))
+    
+    def inc(self, index):
+        if self.start == self.end:
+            self.count += 1
+            return
+        
+        if index <= self.left.end:
+            self.left.inc(index)
+        else:
+            self.right.inc(index)
+        
+        self.count = self.left.count + self.right.count
+
+
+class Solution:
+    """
+    @param A: A list of integer
+    @return: Count the number of element before this element 'ai' is 
+             smaller than it and return count number list
+    """
+    def countOfSmallerNumberII(self, A):
+        if len(A) == 0:
+            return []
+            
+        root = SegTree(0, max(A))
+        
+        results = []
+        for a in A:
+            results.append(root.sum(0, a - 1))
+            root.inc(a)
+        return results   
+
     
