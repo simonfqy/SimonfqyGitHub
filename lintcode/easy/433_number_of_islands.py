c306ecb9a5b713af9acabf66ae7e4e474eeb5f73
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-02 23:51:24 -0800

    Update 433_number_of_islands.py

--- lintcode/easy/433_number_of_islands.py
+++ lintcode/easy/433_number_of_islands.py
@@ -40,5 +40,53 @@ class Solution:
                     continue
                 queue.append((new_row, new_col))
                 visited.add((new_row, new_col))
-
                 
+
+# UnionFind solution from jiuzhang.com.
+class Solution:
+    """
+    @param grid: a boolean 2D matrix
+    @return: an integer
+    """
+    ans = 0
+    pre = []
+    
+    def unionfind(self, root):
+	    son = root;
+	    while(root != self.pre[root]):
+	    	root = self.pre[root]
+	    while(son != root):
+		    tmp = self.pre[son];
+		    self.pre[son] = root
+		    son = tmp
+	    return root
+    
+    def connect(self, A, B):
+        rootA = self.unionfind(A)
+        rootB = self.unionfind(B)
+        if(rootA != rootB):
+            self.pre[rootB] = rootA
+            self.ans -= 1
+    
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        n = len(grid)
+        m = len(grid[0])
+        self.pre = []
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                self.pre += [i * m + j]
+                if grid[i][j]:
+                    self.ans += 1
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]:
+                    # Only need to consider the lower and righter neighbor of the current position, because the upper and left neighbors are visited.
+                    if(i + 1 < n and grid[i + 1][j]):
+                        self.connect(i * m + j + m, i * m + j)
+                    if(j + 1 < m and grid[i][j + 1]):
+                        self.connect(i * m + j + 1, i * m + j)
+        return self.ans
+
+    
