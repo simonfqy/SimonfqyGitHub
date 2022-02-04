baa7dedc598600ee696572756bc4c894fd343b32
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-04 14:11:04 -0800

    Update 547_intersection_of_two_arrays.py

--- lintcode/easy/547_intersection_of_two_arrays.py
+++ lintcode/easy/547_intersection_of_two_arrays.py
@@ -40,3 +40,50 @@ class Solution:
         return list(nums1_set & nums2_set)      
 
     
+# My own solution. Used binary search. Much more complicated than other methods, the time complexity also not good.
+class Solution:
+    """
+    @param nums1: an integer array
+    @param nums2: an integer array
+    @return: an integer array
+    """
+    def intersection(self, nums1, nums2):
+        nums1 = sorted(list(set(nums1)))
+        nums2 = sorted(list(set(nums2)))
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        n, m = len(nums1), len(nums2)
+        ind_in_nums2 = 0
+        res = []
+        for i, element in enumerate(nums1):
+            candidate_ind_in_nums2 = self.get_ind_in_nums(element, nums2, ind_in_nums2, m - 1)
+            if candidate_ind_in_nums2 < 0:
+                continue
+            if candidate_ind_in_nums2 == m:
+                break
+            res.append(element)
+            ind_in_nums2 = candidate_ind_in_nums2 + 1
+        return res
+
+    def get_ind_in_nums(self, val, nums, left, right):        
+        start, end = left, right
+        if start > end:
+            return -2
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] <= val:
+                start = mid
+            else:
+                end = mid
+        if nums[start] > val:
+            return -1
+        if nums[start] == val:
+            return start
+        if nums[end] == val:
+            return end
+        if nums[end] < val:
+            return right + 1
+        return -2
+    
+    
+    
