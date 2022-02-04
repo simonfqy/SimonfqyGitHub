bd7037a79a2f485d0c1584020e249a9c5d278141
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-04 13:31:45 -0800

    Create 547_intersection_of_two_arrays.py

---
+++ lintcode/easy/547_intersection_of_two_arrays.py
@@ -0,0 +1,30 @@
+'''
+Link: https://www.lintcode.com/problem/547/
+'''
+
+
+# Used two pointers on sorted deduplicated lists.
+class Solution:
+    """
+    @param nums1: an integer array
+    @param nums2: an integer array
+    @return: an integer array
+    """
+    def intersection(self, nums1, nums2):
+        nums1 = sorted(list(set(nums1)))
+        nums2 = sorted(list(set(nums2)))
+        i = j = 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+        return res
+      
+      
+      
