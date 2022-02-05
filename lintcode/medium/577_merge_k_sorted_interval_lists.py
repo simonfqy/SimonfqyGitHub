000f1800cd5332ce1e148096a584deceb521cbb0
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-04 23:57:16 -0800

    Update 577_merge_k_sorted_interval_lists.py

--- lintcode/medium/577_merge_k_sorted_interval_lists.py
+++ lintcode/medium/577_merge_k_sorted_interval_lists.py
@@ -44,4 +44,51 @@ class Solution:
         res[-1].end = max(res[-1].end, interval.end)
 
         
+# My own solution, using recursive merging. Time complexity is O(nlogk), where n is the total number of intervals.
+Interval.__lt__ = lambda x, y: (x.start < y.start or (x.start == y.start and x.end < y.end))
+class Solution:
+    """
+    @param intervals: the given k sorted interval lists
+    @return:  the new sorted interval list
+    """
+    def mergeKSortedIntervalLists(self, intervals):
+        k = len(intervals)
+        return self.merge_interval_lists(intervals, 0, k - 1)
+
+    def merge_interval_lists(self, intervals, start, end):
+        if start == end:
+            return intervals[start]
+        mid = (start + end) // 2
+        left = self.merge_interval_lists(intervals, start, mid)
+        right = self.merge_interval_lists(intervals, mid + 1, end)
+        return self.merge_two_lists(left, right)
+
+    def merge_two_lists(self, list_1, list_2):
+        res = []
+        i = j = 0
+        while i < len(list_1) and j < len(list_2):
+            if list_1[i] < list_2[j]:
+                interval = list_1[i]
+                i += 1
+            else:
+                interval = list_2[j]
+                j += 1
+            self.add_interval_to_list(res, interval)
+
+        for ind_1 in range(i, len(list_1)):
+            self.add_interval_to_list(res, list_1[ind_1])
+        for ind_2 in range(j, len(list_2)):
+            self.add_interval_to_list(res, list_2[ind_2])
+        return res            
+    
+    def add_interval_to_list(self, res, interval):
+        if not res:
+            res.append(interval)
+            return
+        if interval.start > res[-1].end:
+            res.append(interval)
+            return
+        res[-1].end = max(res[-1].end, interval.end)        
+
+        
         
