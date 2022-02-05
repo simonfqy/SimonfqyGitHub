55b0a84a2836d964481d03e11e8f5c3e8cd048a7
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-04 23:22:48 -0800

    Update 793_intersection_of_arrays.py

--- lintcode/medium/793_intersection_of_arrays.py
+++ lintcode/medium/793_intersection_of_arrays.py
@@ -119,3 +119,26 @@ class Solution:
         return len(set_1)
     
     
+# Answer from jiuzhang.com. Uses hash map.
+class Solution:
+    """
+    @param arrs: the arrays
+    @return: the number of the intersection of the arrays
+    """
+    def intersectionOfArrays(self, arrs):
+        count = {}
+        # 记录每个数的出现次数
+        for arr in arrs:
+            for x in arr:
+                if x not in count:
+                    count[x] = 0
+                count[x] += 1
+        
+        # 某个数出现次数等于数组个数，代表它在所有数组中都出现过
+        result = 0
+        for x in count.keys():
+            if count[x] == len(arrs):
+                result += 1
+        return result
+    
+    
