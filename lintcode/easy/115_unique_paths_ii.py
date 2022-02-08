c088c0bef473734f77daad94b5176c801dab1fcf
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-07 19:08:31 -0800

    Create 115_unique_paths_ii.py

---
+++ lintcode/easy/115_unique_paths_ii.py
@@ -0,0 +1,35 @@
+'''
+https://www.lintcode.com/problem/115/
+'''
+
+# My own solution. Uses memoized search.
+class Solution:
+    """
+    @param obstacleGrid: A list of lists of integers
+    @return: An integer
+    """
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        if not obstacleGrid or not obstacleGrid[0]:
+            return 0
+        self.grid_cell_to_path = [[None] * len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
+        self.unique_paths(obstacleGrid, 0, 0)
+        return self.grid_cell_to_path[0][0]
+
+    def unique_paths(self, obstacleGrid, row, col):
+        if self.grid_cell_to_path[row][col] is not None:
+            return        
+        if obstacleGrid[row][col] == 1:
+            self.grid_cell_to_path[row][col] = 0
+            return
+        if row == len(obstacleGrid) - 1 and col == len(obstacleGrid[0]) - 1:
+            self.grid_cell_to_path[row][col] = 1
+            return      
+        self.grid_cell_to_path[row][col] = 0
+        if row + 1 < len(obstacleGrid):
+            self.unique_paths(obstacleGrid, row + 1, col)
+            self.grid_cell_to_path[row][col] += self.grid_cell_to_path[row + 1][col]
+        if col + 1 < len(obstacleGrid[0]):
+            self.unique_paths(obstacleGrid, row, col + 1)
+            self.grid_cell_to_path[row][col] += self.grid_cell_to_path[row][col + 1]
+
+            
