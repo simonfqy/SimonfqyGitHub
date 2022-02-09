0ad50f4071ab84df5e63d63b5fd5d4a24db82dfe
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-08 19:51:48 -0800

    Update 117_jump_game_ii.py

--- lintcode/medium/117_jump_game_ii.py
+++ lintcode/medium/117_jump_game_ii.py
@@ -2,7 +2,7 @@
 Link: https://www.lintcode.com/problem/117
 '''
 
-# My own solution. Should be correct, but hits time limit exceeded exception.
+# My own solution. Has O(n) time complexity. Uses greedy algorithm and dynamic programming.
 class Solution:
     """
     @param A: A list of integers
@@ -12,16 +12,17 @@ class Solution:
         n = len(A)
         dp = [float('inf')] * n
         dp[0] = 0
+        # If we don't use this frontier variable to eliminate some futile searches, it will hit time limit exceeded exception.
+        frontier = 0
         for i in range(n):
-            if dp[i] == float('inf'):
+            if i > frontier:
                 break
             max_jump_len = A[i]
-            for j in range(i + 1, i + max_jump_len + 1):
+            for j in range(frontier + 1, i + max_jump_len + 1):
                 if j >= n:
                     break
                 dp[j] = min(dp[j], dp[i] + 1)
+            frontier = max(frontier, i + max_jump_len)
         return dp[n - 1]
-
-
 
   
