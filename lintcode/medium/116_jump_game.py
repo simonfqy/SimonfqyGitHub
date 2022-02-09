8b204f4ee23a29e0408c5af4b390cdbd921f2ed0
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-08 18:54:32 -0800

    Update 116_jump_game.py

--- lintcode/medium/116_jump_game.py
+++ lintcode/medium/116_jump_game.py
@@ -26,3 +26,20 @@ class Solution:
         return can_reach[n - 1]
 
 
+# My own solution. Uses greedy algorithm, has O(n) time complexity.
+class Solution:
+    """
+    @param A: A list of integers
+    @return: A boolean
+    """
+    def canJump(self, A):
+        n = len(A)
+        reachable_frontier = 0
+        for i in range(n):
+            if i > reachable_frontier:
+                break
+            reachable_frontier = max(reachable_frontier, i + A[i])
+        return reachable_frontier >= n - 1
+    
+    
+    
