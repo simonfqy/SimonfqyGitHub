873a1454f50d15352936058801dbb82dc5ff2f84
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-09 14:49:21 -0800

    Update 76_longest_increasing_subsequence.py

--- lintcode/medium/76_longest_increasing_subsequence.py
+++ lintcode/medium/76_longest_increasing_subsequence.py
@@ -22,4 +22,43 @@ class Solution:
                 dp[i] = max(dp[i], dp[j] + 1)
                 
         return max(dp)
+    
         
+# Instructed by jiuzhang.com, implemented by myself. DP with binary search, has O(nlogn) time complexity.
+class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        if not nums:
+            return 0    
+        # Each element lis_len_to_smallest_ending_num[j] is the smallest ending number of any LIS with length j.
+        # This array is monotonically increasing.
+        lis_len_to_smallest_ending_num = [float('-inf'), nums[0]]
+        for i, num in enumerate(nums):
+            # We need to find the last element in lis_len_to_smallest_ending_num array which is smaller than nums[i].
+            last_smaller_element_ind = self.get_last_smaller_element_ind(lis_len_to_smallest_ending_num, num)
+            ind_to_overwrite = last_smaller_element_ind + 1
+            if len(lis_len_to_smallest_ending_num) == ind_to_overwrite:
+                lis_len_to_smallest_ending_num.append(float('inf'))
+            # Update the element after the last_smaller_element_ind in the array if nums[i] is smaller than it. 
+            lis_len_to_smallest_ending_num[ind_to_overwrite] = min(lis_len_to_smallest_ending_num[ind_to_overwrite], num)            
+        return len(lis_len_to_smallest_ending_num) - 1
+
+    
+    def get_last_smaller_element_ind(self, array, num):
+        left, right = 0, len(array) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if array[mid] < num:
+                left = mid
+            else:
+                right = mid
+        if array[right] < num:
+            return right
+        if array[left] < num:
+            return left
+        return left - 1
+
+    
