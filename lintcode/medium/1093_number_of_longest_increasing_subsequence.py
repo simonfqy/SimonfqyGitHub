e31d5f6f083fdc1c5a6184d5e51c8000737ca87c
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-09 19:16:42 -0800

    Update 1093_number_of_longest_increasing_subsequence.py

--- lintcode/medium/1093_number_of_longest_increasing_subsequence.py
+++ lintcode/medium/1093_number_of_longest_increasing_subsequence.py
@@ -61,4 +61,33 @@ class Solution:
         return max_counter
 
 
+# Slightly modified from the one above, only traversing once rather than twice. 
+class Solution:
+    """
+    @param nums: an array
+    @return: the number of longest increasing subsequence
+    """
+    def findNumberOfLIS(self, nums):
+        n = len(nums)
+        # If we use [[1, 1]] * n, it will cause issues later. For example, if n == 2, nd we later let array[1][0] += 3, then the array becomes [[4, 1], [4, 1]].
+        length_and_count_of_LIS_ending_at_i = [[1, 1] for _ in range(n)]
+        max_counter = 0
+        max_LIS_len = 1
+        for i in range(n):
+            for j in range(i):
+                if nums[i] <= nums[j]:
+                    continue
+                if length_and_count_of_LIS_ending_at_i[i][0] < length_and_count_of_LIS_ending_at_i[j][0] + 1:
+                    length_and_count_of_LIS_ending_at_i[i] = [length_and_count_of_LIS_ending_at_i[j][0] + 1, length_and_count_of_LIS_ending_at_i[j][1]]                  
+                    
+                elif length_and_count_of_LIS_ending_at_i[i][0] == length_and_count_of_LIS_ending_at_i[j][0] + 1:
+                    length_and_count_of_LIS_ending_at_i[i][1] = length_and_count_of_LIS_ending_at_i[i][1] + length_and_count_of_LIS_ending_at_i[j][1]
+            if max_LIS_len < length_and_count_of_LIS_ending_at_i[i][0]:
+                max_LIS_len = length_and_count_of_LIS_ending_at_i[i][0]
+                max_counter = 0
+            if max_LIS_len == length_and_count_of_LIS_ending_at_i[i][0]:
+                max_counter += length_and_count_of_LIS_ending_at_i[i][1]
+                    
+        return max_counter
+
     
