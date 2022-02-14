b054abab77c6b3b779a19f8ca59afa5c06e1de65
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-14 00:38:31 -0800

    Create 603_largest_divisible_subset.py

---
+++ lintcode/medium/603_largest_divisible_subset.py
@@ -0,0 +1,42 @@
+'''
+Link: https://www.lintcode.com/problem/603/
+'''
+
+
+# My own solution. Has O(n^2) time complexity. Should be correct, but causes time limit exceeded exception.
+class Solution:
+    """
+    @param nums: a set of distinct positive integers
+    @return: the largest subset 
+    """
+    def largestDivisibleSubset(self, nums):
+        n = len(nums)
+        if n < 2:
+            return []
+        nums_list = sorted(list(nums))
+        subsets_at_each_ind = [[set([nums_list[0]])]]        
+        max_subset_size = 1
+        max_subset = [nums_list[0]]
+        for i in range(1, n):
+            end_num = nums_list[i]
+            subsets_at_curr_ind = []
+            nums_we_should_not_visit = set()
+            for j in range(i - 1, -1, -1):
+                curr_num = nums_list[j]
+                if curr_num > end_num // 2 or end_num % curr_num != 0:
+                    continue
+                if curr_num in nums_we_should_not_visit:
+                    continue
+                for subset in subsets_at_each_ind[j]:
+                    updated_subset = subset | {end_num}
+                    subsets_at_curr_ind.append(updated_subset)
+                    if len(updated_subset) > max_subset_size:
+                        max_subset_size = len(updated_subset)
+                        max_subset = sorted(list(updated_subset))
+                    nums_we_should_not_visit |= subset
+            subsets_at_each_ind.append(subsets_at_curr_ind)          
+
+        return max_subset
+
+        
+        
