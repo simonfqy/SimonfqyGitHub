959ab73fc6962c02b16513d749efb35885cc54ba
Qingyuan Feng <simonfengqy@gmail.com>
2022-02-28 19:31:22 -0800

    Update 1262_utf_8_validation.py

--- lintcode/medium/1262_utf_8_validation.py
+++ lintcode/medium/1262_utf_8_validation.py
@@ -42,4 +42,31 @@ class Solution:
                 return count
             count += 1
         return count
+    
 
+# Solution from jiuzhang.com. 
+def check(nums, start, size):
+    for i in range(start + 1, start + size + 1):
+        if i >= len(nums) or (nums[i] >> 6) != 0b10:
+            return False
+    return True
+
+class Solution(object):
+    def validUtf8(self, nums, start=0):
+        if not nums:
+            return False
+        while start < len(nums):
+            first = nums[start]
+            if (first >> 3) == 0b11110 and check(nums, start, 3):
+                start += 4
+            elif (first >> 4) == 0b1110  and check(nums, start, 2): 
+                start += 3
+            elif (first >> 5) == 0b110   and check(nums, start, 1): 
+                start += 2
+            elif (first >> 7) == 0:
+                start += 1
+            else:
+                return False
+        return True
+    
+    
