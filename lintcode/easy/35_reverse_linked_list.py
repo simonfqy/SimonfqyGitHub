d618a16760df2bbb57e27be40f8fac5a356ea845
Qingyuan Feng <simonfengqy@gmail.com>
2022-03-20 18:03:37 -0700

    Create 35_reverse_linked_list.py

---
+++ lintcode/easy/35_reverse_linked_list.py
@@ -0,0 +1,32 @@
+'''
+Link: https://www.lintcode.com/problem/35
+'''
+
+from lintcode import (
+    ListNode,
+)
+
+"""
+Definition of ListNode:
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+# My own solution. Modifies the list in-place and goes in 1-pass. Has O(n) time and O(1) space complexities.
+class Solution:
+    """
+    @param head: n
+    @return: The new head of reversed linked list.
+    """
+    def reverse(self, head: ListNode) -> ListNode:
+        head_of_reversed_list = None
+        cursor = head
+        while cursor:
+            next_node = cursor.next
+            new_head = cursor
+            new_head.next = head_of_reversed_list
+            head_of_reversed_list = new_head
+            cursor = next_node
+        return head_of_reversed_list
