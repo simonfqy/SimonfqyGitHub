82c03967fc05266b75757e0a4c73cf579050e4df
Qingyuan Feng <simonfengqy@gmail.com>
2022-03-20 17:44:37 -0700

    Update 5_kth_largest_element.py

--- lintcode/medium/5_kth_largest_element.py
+++ lintcode/medium/5_kth_largest_element.py
@@ -63,6 +63,7 @@ class Solution:
         left, right = start, end
         pivot = nums[(start + end) // 2]
         while left <= right:
+            # Here we have to use < or > pivot, cannot use <= or >= pivot. Otherwise it will encounter infinite function calls.
             while left <= right and nums[left] < pivot:
                 left += 1
             while left <= right and nums[right] > pivot:
@@ -71,7 +72,7 @@ class Solution:
                 nums[left], nums[right] = nums[right], nums[left]
                 left, right = left + 1, right - 1
                 
-        # left is now bigger than right
+        # left is now bigger than right. Here we have to use k <= and k >=, cannot drop the equal sign. Otherwise it produces wrong output.
         if k <= right:
             return self.partition(nums, start, right, k)
         if k >= left:
