939cfed2fd0280e867194971f568a96879c9dd6b
Qingyuan Feng <simonfengqy@gmail.com>
2022-03-27 20:13:23 -0700

    Create 178_graph_valid_tree.py

---
+++ lintcode/medium/178_graph_valid_tree.py
@@ -0,0 +1,35 @@
+'''
+Link: https://www.lintcode.com/problem/178/
+'''
+
+from typing import (
+    List,
+)
+
+# My implementation of the solution from jiuzhang.com. It takes advantage of the fact that a completely connected tree with n nodes 
+# without cycles or duplicate edges should have n - 1 edges, and we should visit every node when doing BFS.
+from collections import deque, defaultdict
+class Solution:
+    # @param {int} n an integer
+    # @param {int[][]} edges a list of undirected edges
+    # @return {boolean} true if it's a valid tree, or false
+    def validTree(self, n, edges):
+        # Write your code here
+        if len(edges) != n - 1:
+            return False
+        node_to_neighbors = defaultdict(list)
+        for node_1, node_2 in edges:
+            node_to_neighbors[node_1].append(node_2)
+            node_to_neighbors[node_2].append(node_1)
+        queue = deque([0])
+        visited = set([0])
+        while queue:
+            node = queue.popleft()
+            for neighbor in node_to_neighbors[node]:
+                if neighbor in visited:
+                    continue
+                visited.add(neighbor)
+                queue.append(neighbor)
+        return len(visited) == n
+      
+      
