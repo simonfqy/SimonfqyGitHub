4d9cb140e9d01d3836ee2604270c0e3187dfad8e
Qingyuan Feng <simonfengqy@gmail.com>
2022-03-30 00:30:16 -0700

    Update 1271_critical_connections_in_a_network.py

--- lintcode/hard/1271_critical_connections_in_a_network.py
+++ lintcode/hard/1271_critical_connections_in_a_network.py
@@ -6,7 +6,9 @@ from typing import (
     List,
 )
 
-# Solution from a student on jiuzhang.com. 
+# Solution from a student on jiuzhang.com. 整体的思路就是，把带环的删掉，剩下的就是关键链接。具体方法就是，
+# 用dfs的层级来代表啥时候visit过，如果发现之前就visit过，那么说明有环，并且整个环的全部节点层级都设定为环上诸节点中最小的层级。
+# 这样我们就可以通过某节点层级与其当前dfs深度的对比，得知该节点是否在环上。如果不在环上，将其加入result中。
 class Solution:
     """
     @param n: the number of servers
