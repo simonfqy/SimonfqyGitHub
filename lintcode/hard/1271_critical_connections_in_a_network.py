7c0c26a6ca35534f94bfcdb2b4f554c9dc45f0ef
Qingyuan Feng <simonfengqy@gmail.com>
2022-03-30 00:49:32 -0700

    Update 1271_critical_connections_in_a_network.py

--- lintcode/hard/1271_critical_connections_in_a_network.py
+++ lintcode/hard/1271_critical_connections_in_a_network.py
@@ -8,7 +8,7 @@ from typing import (
 
 # Solution from a student on jiuzhang.com. 整体的思路就是，把带环的删掉，剩下的就是关键链接。具体方法就是，
 # 用dfs的层级来代表啥时候visit过，如果发现之前就visit过，那么说明有环，并且整个环的全部节点层级都设定为环上诸节点中最小的层级。
-# 这样我们就可以通过某节点层级与其当前dfs深度的对比，得知该节点是否在环上。如果不在环上，将其加入result中。
+# 这样我们就可以通过某节点层级与其当前dfs深度的对比，得知某边是否在环上。如果不在环上，将其加入result中。
 class Solution:
     """
     @param n: the number of servers
@@ -30,12 +30,20 @@ class Solution:
         for child in node_to_neighbors[node]:
             if child == father:
                 continue
+            # We use DFS and traverse through all the childs of the current node. This means that after the for loop finishes,
+            # if any node-child edge is a part of a cycle, ranks[child] < depth + 1. This is because the child will eventually
+            # reach node in its own DFS, and the ranks[child] will be <= depth. This means that when we call dfs_return_minimum_rank()
+            # on the child, the ranks[child] < depth + 1.
+            # It has implications on the next if block after the for loop.
             if ranks[child] < 0:
                 ranks[node] = min(ranks[node], self.dfs_return_minimum_rank(child, node, 
                         depth + 1, ranks, critical_connections, node_to_neighbors))
             else:
                 ranks[node] = min(ranks[node], ranks[child])
+        # If father-node edge is a part of a cycle, it should have ranks[node] == depth. See the documentation inside the if-block.
         if node != 0 and ranks[node] == depth:
             critical_connections.add(tuple(sorted((father, node))))
         return ranks[node]
 
+
+    
