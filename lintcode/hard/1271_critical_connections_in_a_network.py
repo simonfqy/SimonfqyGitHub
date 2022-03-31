34e1796ca56960f640e4020340bed28452cdb8e3
Qingyuan Feng <simonfengqy@gmail.com>
2022-03-30 17:26:51 -0700

    Update 1271_critical_connections_in_a_network.py

--- lintcode/hard/1271_critical_connections_in_a_network.py
+++ lintcode/hard/1271_critical_connections_in_a_network.py
@@ -47,4 +47,36 @@ class Solution:
         return ranks[node]
 
 
+# Another implementation of Tarjan's algorithm. Learned from Youtube.
+class Solution:
+    """
+    @param n: the number of servers
+    @param connections: connections
+    @return: Critical Connections in a Network
+    """
+    def critical_connectionsina_network(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        node_to_neighbors = [[] for _ in range(n)]
+        for node_a, node_b in connections:
+            node_to_neighbors[node_a].append(node_b)
+            node_to_neighbors[node_b].append(node_a)
+        ranks = [float('-inf')] * n
+        critical_connections = set()
+        self.dfs_return_minimum_rank(0, -1, 0, ranks, critical_connections, node_to_neighbors)
+        return list([list(connection) for connection in critical_connections])
+    
+    def dfs_return_minimum_rank(self, node, father, depth, ranks, critical_connections, node_to_neighbors):
+        ranks[node] = depth
+        for child in node_to_neighbors[node]:
+            if child == father:
+                continue
+            if ranks[child] < 0:
+                ranks[node] = min(ranks[node], self.dfs_return_minimum_rank(child, node, 
+                        depth + 1, ranks, critical_connections, node_to_neighbors))
+                # This is the only place where the difference exists compared to the solution above. 
+                # Only if ranks[child] > depth, can we be certain that the node-child edge is a critical connection.
+                if ranks[child] > depth:
+                    critical_connections.add(tuple(sorted((child, node))))
+            else:
+                ranks[node] = min(ranks[node], ranks[child])        
+        return ranks[node]
     
