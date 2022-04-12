2c8ac6c90b963a1ed8d9392ca24aede9f6f2b2c3
Qingyuan Feng <simonfengqy@gmail.com>
2022-04-11 23:33:57 -0700

    Update 1064_my_calendar_ii.py

--- lintcode/medium/1064_my_calendar_ii.py
+++ lintcode/medium/1064_my_calendar_ii.py
@@ -19,8 +19,8 @@ class MyCalendarTwo:
         updated_events = list(self.events)
         start_event = (start, 1)
         end_event = (end, -1)
-        self.add_to_events_list(updated_events, start_event)
-        self.add_to_events_list(updated_events, end_event)
+        bisect.insort(updated_events, start_event)
+        bisect.insort(updated_events, end_event)
         current_event_count = 0
         for time, delta in updated_events:
             if time >= end:
@@ -30,10 +30,6 @@ class MyCalendarTwo:
                 return False
         self.events = updated_events
         return True
-    
-    def add_to_events_list(self, updated_events, event):
-        ind = bisect.bisect_left(updated_events, event)
-        updated_events.insert(ind, event)
         
 
         
