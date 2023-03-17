40a4704833b0348814164b665b6736e5b6519ec0
Qingyuan Feng <simonfengqy@gmail.com>
2023-03-17 13:43:53 -0700

    Update 778_pacific_atlantic_water_flow.py

--- lintcode/medium/778_pacific_atlantic_water_flow.py
+++ lintcode/medium/778_pacific_atlantic_water_flow.py
@@ -53,7 +53,8 @@ class Solution:
         return False  
       
       
-# My solution based on the teachings from jiuzhang.com.    
+# My solution based on the teachings from jiuzhang.com. Performing BFS from the edges to the centre, and taking
+# intersection at the end.
 from collections import deque
 DELTA = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 class Solution:
@@ -97,4 +98,49 @@ class Solution:
                 water_source.add((new_x, new_y))
                 queue.append((new_x, new_y))  
                 
-                
+   
+# My own solution based on the teachings from jiuzhang.com. Taking DFS from edges to the centre.
+DELTA = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+class Solution:
+    """
+    @param matrix: the given matrix
+    @return: The list of grid coordinates
+             we will sort your return value in output
+    """
+    def pacific_atlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+        atlantic_sources = set()
+        pacific_sources = set()
+        if not matrix or not matrix[0]:
+            return []
+        n, m = len(matrix), len(matrix[0])
+        atlantic_visited = [[False for _ in range(m)] for _ in range(n)]
+        pacific_visited = [[False for _ in range(m)] for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if i == 0 or j == 0:
+                    atlantic_visited[i][j] = True
+                    self.get_water_source(matrix, n, m, i, j, atlantic_sources, atlantic_visited)
+                    atlantic_visited[i][j] = False
+                if i == n - 1 or j == m - 1:
+                    pacific_visited[i][j] = True
+                    self.get_water_source(matrix, n, m, i, j, pacific_sources, pacific_visited)
+                    pacific_visited[i][j] = False
+        results_set = atlantic_sources & pacific_sources
+        return [list(tup) for tup in results_set]
+
+    def get_water_source(self, matrix, n, m, i, j, water_source, visited):
+        water_source.add((i, j))       
+        prev_height = matrix[i][j]
+        for delta_x, delta_y in DELTA:
+            new_x, new_y = i + delta_x, j + delta_y
+            if min(new_x, new_y) < 0 or new_x >= n or new_y >= m:
+                continue
+            if matrix[new_x][new_y] < prev_height:
+                continue
+            if visited[new_x][new_y] or (new_x, new_y) in water_source:
+                continue
+            visited[new_x][new_y] = True
+            self.get_water_source(matrix, n, m, new_x, new_y, water_source, visited)
+            visited[new_x][new_y] = False 
+            
+            
