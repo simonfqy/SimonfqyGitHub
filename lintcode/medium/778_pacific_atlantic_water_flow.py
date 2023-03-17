8ac665cf1637113b284422989cc26606798130aa
Qingyuan Feng <simonfengqy@gmail.com>
2023-03-17 13:22:06 -0700

    Update 778_pacific_atlantic_water_flow.py

--- lintcode/medium/778_pacific_atlantic_water_flow.py
+++ lintcode/medium/778_pacific_atlantic_water_flow.py
@@ -53,3 +53,48 @@ class Solution:
         return False  
       
       
+# My solution based on the teachings from jiuzhang.com.    
+from collections import deque
+DELTA = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+class Solution:
+    """
+    @param matrix: the given matrix
+    @return: The list of grid coordinates
+             we will sort your return value in output
+    """
+    def pacific_atlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+        atlantic_sources = set()
+        pacific_sources = set()
+        if not matrix or not matrix[0]:
+            return []
+        n, m = len(matrix), len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if i == 0 or j == 0:
+                    self.get_water_source(matrix, n, m, i, j, atlantic_sources)
+                if i == n - 1 or j == m - 1:
+                    self.get_water_source(matrix, n, m, i, j, pacific_sources)
+        results_set = atlantic_sources & pacific_sources
+        return [list(tup) for tup in results_set]
+
+    def get_water_source(self, matrix, n, m, i, j, water_source):
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        queue = deque([(i, j)])
+        visited[i][j] = True
+        water_source.add((i, j))
+        while queue:
+            x, y = queue.popleft()
+            prev_height = matrix[x][y]
+            for delta_x, delta_y in DELTA:
+                new_x, new_y = x + delta_x, y + delta_y
+                if min(new_x, new_y) < 0 or new_x >= n or new_y >= m:
+                    continue
+                if matrix[new_x][new_y] < prev_height:
+                    continue
+                if visited[new_x][new_y] or (new_x, new_y) in water_source:
+                    continue
+                visited[new_x][new_y] = True
+                water_source.add((new_x, new_y))
+                queue.append((new_x, new_y))  
+                
+                
