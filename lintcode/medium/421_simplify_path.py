9498e0e4c402d8ab3330e0515ba83069f536faa5
Qingyuan Feng <simonfengqy@gmail.com>
2023-03-16 18:00:52 -0700

    Update 421_simplify_path.py

--- lintcode/medium/421_simplify_path.py
+++ lintcode/medium/421_simplify_path.py
@@ -4,7 +4,12 @@ This problem was asked in Lotusflare's 1st round technical interview on Mar 16, 
 it using the best method.
 '''
 
-# My own solution. I arrived at this solution after getting the hint from the interviewer.
+# My own solution. Uses stack. I arrived at this solution after getting the hint from the interviewer.
+# I originally understood this problem as a string comprehension and manipulation problem. I didn't realize that
+# splitting based on '/' and getting a list of path entities can be useful. Nor did I realize this was a stack
+# related problem. So I demonstrated a lack of abstraction ability in solving this problem. In the future I should
+# think harder about how to construct a model to accurately abstract the problem, instead of directly working on the
+# concrete low-level data.
 class Solution:
     """
     @param path: the original path
