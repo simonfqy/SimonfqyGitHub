0b4946af6a318832b8ec5a7aa8488ce48b789195
Qingyuan Feng <simonfengqy@gmail.com>
2023-03-19 15:56:38 -0700

    Update 102_linked_list_cycle.py

--- lintcode/medium/102_linked_list_cycle.py
+++ lintcode/medium/102_linked_list_cycle.py
@@ -46,3 +46,21 @@ class Solution:
             node_set.add(current)
             current = current.next
         return False
+
+   
+# Another two pointer solution. Simpler than the one above.
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @return: True if it has a cycle, or false
+    """
+    def hasCycle(self, head):
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        return False
+    
+    
